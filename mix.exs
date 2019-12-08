dbf90469d538ea0f124a65e514f34167894e9f1d
Zach Daniel <zachary.s.daniel@gmail.com>
2019-12-08 14:05:38 -0500

    WIP

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule Ash.MixProject do
   def project do
     [
       app: :ash,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
