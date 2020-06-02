58d9adc6bd38e6a740b5cec2137715eb3f597a3b
Zach Daniel <zachary.s.daniel@gmail.com>
2020-06-01 22:09:26 -0400

    fix: remove test warning from git_ops

--- config/config.exs
+++ config/config.exs
@@ -1,13 +1,15 @@
 use Mix.Config
 
-config :git_ops,
-  mix_project: Ash.MixProject,
-  changelog_file: "CHANGELOG.md",
-  repository_url: "https://github.com/ash-project/ash",
-  # Instructs the tool to manage your mix version in your `mix.exs` file
-  # See below for more information
-  manage_mix_version?: true,
-  # Instructs the tool to manage the version in your README.md
-  # Pass in `true` to use `"README.md"` or a string to customize
-  manage_readme_version: "README.md",
-  version_tag_prefix: "v"
+if Mix.env() == :dev do
+  config :git_ops,
+    mix_project: Ash.MixProject,
+    changelog_file: "CHANGELOG.md",
+    repository_url: "https://github.com/ash-project/ash",
+    # Instructs the tool to manage your mix version in your `mix.exs` file
+    # See below for more information
+    manage_mix_version?: true,
+    # Instructs the tool to manage the version in your README.md
+    # Pass in `true` to use `"README.md"` or a string to customize
+    manage_readme_version: "README.md",
+    version_tag_prefix: "v"
+end
