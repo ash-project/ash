945232500f24df4bbe32988265f613dd9200481f
Zach Daniel <zachary.s.daniel@gmail.com>
2020-06-02 09:24:06 -0400

    ci: fix preferred CLI env

--- mix.exs
+++ mix.exs
@@ -21,9 +21,7 @@ defmodule Ash.MixProject do
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
         coveralls: :test,
-        "coveralls.detail": :test,
-        "coveralls.post": :test,
-        "coveralls.html": :test
+        "coveralls.github": :test
       ],
       docs: docs(),
       aliases: aliases(),
