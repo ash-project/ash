846f381d08cd1843c8f02ee9fe068f784236ca9a
Mike Binns <TheFirstAvenger@users.noreply.github.com>
2020-08-25 15:39:40 -0400

    ci: Improve CI Elixir/Erlang matrix (#96)

--- lib/ash/actions/sort.ex
+++ lib/ash/actions/sort.ex
@@ -59,15 +59,41 @@ defmodule Ash.Actions.Sort do
   def runtime_sort(results, empty) when empty in [nil, []], do: results
 
   def runtime_sort(results, [{field, direction}]) do
-    Enum.sort_by(results, &Map.get(&1, field), direction)
+    sort_by(results, &Map.get(&1, field), direction)
   end
 
   def runtime_sort(results, [{field, direction} | rest]) do
     results
     |> Enum.group_by(&Map.get(&1, field))
-    |> Enum.sort_by(fn {key, _value} -> key end, direction)
+    |> sort_by(fn {key, _value} -> key end, direction)
     |> Enum.flat_map(fn {_, records} ->
       runtime_sort(records, rest)
     end)
   end
+
+  # :asc/:desc added to elixir in 1.10. sort_by and to_sort_by_fun copied from core
+  defp sort_by(enumerable, mapper, sorter) do
+    enumerable
+    |> Enum.map(&{&1, mapper.(&1)})
+    |> Enum.sort(to_sort_by_fun(sorter))
+    |> Enum.map(&elem(&1, 0))
+  end
+
+  defp to_sort_by_fun(sorter) when is_function(sorter, 2),
+    do: &sorter.(elem(&1, 1), elem(&2, 1))
+
+  defp to_sort_by_fun(:asc),
+    do: &(elem(&1, 1) <= elem(&2, 1))
+
+  defp to_sort_by_fun(:desc),
+    do: &(elem(&1, 1) >= elem(&2, 1))
+
+  defp to_sort_by_fun(module) when is_atom(module),
+    do: &(module.compare(elem(&1, 1), elem(&2, 1)) != :gt)
+
+  defp to_sort_by_fun({:asc, module}) when is_atom(module),
+    do: &(module.compare(elem(&1, 1), elem(&2, 1)) != :gt)
+
+  defp to_sort_by_fun({:desc, module}) when is_atom(module),
+    do: &(module.compare(elem(&1, 1), elem(&2, 1)) != :lt)
 end
