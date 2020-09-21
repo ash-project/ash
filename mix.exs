0f34797ee22bedbf7a413554169776f2561f7d46
Zach Daniel <zachary.s.daniel@gmail.com>
2020-09-21 18:33:34 -0400

    fix: upgrade picosat dependency

--- mix.exs
+++ mix.exs
@@ -110,7 +110,7 @@ defmodule Ash.MixProject do
       {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
       {:sobelow, ">= 0.0.0", only: :dev, runtime: false},
       {:git_ops, "~> 2.0.1", only: :dev},
-      {:picosat_elixir, "~> 0.1.4"},
+      {:picosat_elixir, "~> 0.1.5"},
       {:nimble_options, "~> 0.3.0"},
       {:excoveralls, "~> 0.13.0", only: [:dev, :test]},
       {:mix_test_watch, "~> 1.0", only: :dev, runtime: false}
