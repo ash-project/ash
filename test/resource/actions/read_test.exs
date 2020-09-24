c2504c3852d3fa74edd9a41da61e2b3c91bdbdfb
Mario Mazo <buster.mazo@gmail.com>
2020-09-24 21:39:00 +0200

    module doc false in tests

--- test/resource/actions/read_test.exs
+++ test/resource/actions/read_test.exs
@@ -1,9 +1,11 @@
 defmodule Ash.Test.Dsl.Resource.Actions.ReadTest do
+  @moduledoc false
   use ExUnit.Case, async: true
 
   defmacrop defposts(do: body) do
     quote do
       defmodule Post do
+        @moduledoc false
         use Ash.Resource,
           data_layer: Ash.DataLayer.Ets
 
