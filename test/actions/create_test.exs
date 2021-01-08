800908793538fbd413f7b3ef50520c58b759e670
zimt28 <1764689+zimt28@users.noreply.github.com>
2021-01-09 00:23:40 +0100

    Correct test

--- test/actions/create_test.exs
+++ test/actions/create_test.exs
@@ -545,7 +545,7 @@ defmodule Ash.Test.Actions.CreateTest do
     end
 
     test "it honors item constraints" do
-      assert_raise Ash.Error.Invalid, ~r/must be less than 10 at index 0/, fn ->
+      assert_raise Ash.Error.Invalid, ~r/must be less than or equal to 10 at index 0/, fn ->
         list = [28, 2, 4]
 
         Post
