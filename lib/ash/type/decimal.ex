db6bdfcf1518c70d67975efb140cdeb2cb6a76e7
zimt28 <1764689+zimt28@users.noreply.github.com>
2021-01-09 17:58:52 +0100

    improvement: Add built in decimal type (#162)

---
+++ lib/ash/type/decimal.ex
@@ -0,0 +1,80 @@
+defmodule Ash.Type.Decimal do
+  @constraints [
+    max: [
+      type: {:custom, __MODULE__, :decimal, []},
+      doc: "Enforces a maximum on the value"
+    ],
+    min: [
+      type: {:custom, __MODULE__, :decimal, []},
+      doc: "Enforces a minimum on the value"
+    ]
+  ]
+  @moduledoc """
+  Represents a decimal.
+
+  A builtin type that can be referenced via `:decimal`
+
+  ### Constraints
+
+  #{Ash.OptionsHelpers.docs(@constraints)}
+  """
+  use Ash.Type
+
+  @impl true
+  def storage_type, do: :decimal
+
+  @impl true
+  def constraints, do: @constraints
+
+  @doc false
+  def decimal(value) do
+    case cast_input(value) do
+      {:ok, decimal} ->
+        {:ok, decimal}
+
+      :error ->
+        {:error, "cannot be casted to decimal"}
+    end
+  end
+
+  def apply_constraints(nil, _), do: :ok
+
+  def apply_constraints(value, constraints) do
+    errors =
+      Enum.reduce(constraints, [], fn
+        {:max, max}, errors ->
+          if Decimal.compare(value, max) == :gt do
+            [{"must be less than or equal to %{max}", max: max} | errors]
+          else
+            errors
+          end
+
+        {:min, min}, errors ->
+          if Decimal.compare(value, min) == :lt do
+            [{"must be more than or equal to %{min}", min: min} | errors]
+          else
+            errors
+          end
+      end)
+
+    case errors do
+      [] -> :ok
+      errors -> {:error, errors}
+    end
+  end
+
+  @impl true
+  def cast_input(value) do
+    Ecto.Type.cast(:decimal, value)
+  end
+
+  @impl true
+  def cast_stored(value) do
+    Ecto.Type.load(:decimal, value)
+  end
+
+  @impl true
+  def dump_to_native(value) do
+    Ecto.Type.dump(:decimal, value)
+  end
+end
