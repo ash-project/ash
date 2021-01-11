b56c841a11f170ea5e07c67a34cd0e894dcea1b2
zimt28 <1764689+zimt28@users.noreply.github.com>
2021-01-11 19:57:51 +0100

    Add constraints for handling empty values

--- lib/ash/type/string.ex
+++ lib/ash/type/string.ex
@@ -1,4 +1,8 @@
 defmodule Ash.Type.String do
+  @empty_characters ["\u00A0", "\u2000", "\u2001", "\u2002", "\u2003"] ++
+                      ["\u2004", "\u2005", "\u2006", "\u2007", "\u2008"] ++
+                      ["\u2009", "\u200A", "\u2028", "\u205F", "\u3000"]
+
   @constraints [
     max_length: [
       type: :non_neg_integer,
@@ -11,6 +15,24 @@ defmodule Ash.Type.String do
     match: [
       type: {:custom, __MODULE__, :match, []},
       doc: "Enforces that the string matches a passed in regex"
+    ],
+    empty_values: [
+      type: :any,
+      doc:
+        "A list of characters which get replaced by a value defined in empty_value_replacement. " <>
+          "To avoid replacing any values, set to an empty list",
+      default: @empty_characters
+    ],
+    empty_value_replacement: [
+      type: :binary,
+      doc:
+        "A value that is used to replace any values defined in empty_values. Defaults to a single space",
+      default: "\u0020"
+    ],
+    trim_value?: [
+      type: :boolean,
+      doc: "Trims the value and sets it to nil if it's empty",
+      default: true
     ]
   ]
 
@@ -34,6 +56,18 @@ defmodule Ash.Type.String do
   def apply_constraints(nil, _), do: :ok
 
   def apply_constraints(value, constraints) do
+    value =
+      Enum.reduce(
+        constraints[:empty_values],
+        value,
+        &String.replace(&2, &1, constraints[:empty_value_replacement])
+      )
+
+    trim_value? = constraints[:trim_value?]
+    trimmed_value = String.trim(value)
+
+    value = (trim_value? && trimmed_value) || value
+
     errors =
       Enum.reduce(constraints, [], fn
         {:max_length, max_length}, errors ->
@@ -56,10 +90,20 @@ defmodule Ash.Type.String do
           else
             [{"must match the pattern %{regex}", regex: inspect(regex)} | errors]
           end
+
+        _, errors ->
+          errors
       end)
 
+    value =
+      if trim_value? && value == "" do
+        nil
+      else
+        value
+      end
+
     case errors do
-      [] -> :ok
+      [] -> {:ok, value}
       errors -> {:error, errors}
     end
   end
