ed155fc97300b18c0775994adac5a5e3243914de
zimt28 <1764689+zimt28@users.noreply.github.com>
2021-01-11 23:19:54 +0100

    Error if relationship is primary key but not required

--- lib/ash/resource/transformers/belongs_to_attribute.ex
+++ lib/ash/resource/transformers/belongs_to_attribute.ex
@@ -20,34 +20,47 @@ defmodule Ash.Resource.Transformers.BelongsToAttribute do
       |> Enum.find(&(Map.get(&1, :name) == relationship.source_field))
     end)
     |> Enum.reduce_while({:ok, dsl_state}, fn relationship, {:ok, dsl_state} ->
-      case Transformer.build_entity(@extension, [:attributes], :attribute,
-             name: relationship.source_field,
-             type: relationship.field_type,
-             allow_nil?:
-               if relationship.primary_key? do
-                 false
-               else
-                 not relationship.required?
-               end,
-             writable?: false,
-             private?: true,
-             primary_key?: relationship.primary_key?
-           ) do
-        {:ok, attribute} ->
-          {:cont, {:ok, Transformer.add_entity(dsl_state, [:attributes], attribute)}}
+      entity =
+        Transformer.build_entity(@extension, [:attributes], :attribute,
+          name: relationship.source_field,
+          type: relationship.field_type,
+          allow_nil?:
+            if relationship.primary_key? do
+              false
+            else
+              not relationship.required?
+            end,
+          writable?: false,
+          private?: true,
+          primary_key?: relationship.primary_key?
+        )
 
-        {:error, error} ->
-          {:halt,
-           {:error,
-            DslError.exception(
-              module: __MODULE__,
-              message:
-                "Could not create attribute for belongs_to #{relationship.name}: #{inspect(error)}",
-              path: [:relationships, relationship.name]
-            )}}
-      end
+      valid_opts? = not (relationship.primary_key? && !relationship.required?)
+
+      entity_or_error =
+        if valid_opts? do
+          entity
+        else
+          {:error, "Relationship cannot be a primary key unless it is also marked as required"}
+        end
+
+      add_entity(entity_or_error, dsl_state, relationship)
     end)
   end
+
+  defp add_entity({:ok, attribute}, dsl_state, _relationship),
+    do: {:cont, {:ok, Transformer.add_entity(dsl_state, [:attributes], attribute)}}
+
+  defp add_entity({:error, error}, _dsl_state, relationship),
+    do:
+      {:halt,
+       {:error,
+        DslError.exception(
+          module: __MODULE__,
+          message:
+            "Could not create attribute for belongs_to #{relationship.name}: #{inspect(error)}",
+          path: [:relationships, relationship.name]
+        )}}
 
   def after?(Ash.Resource.Transformers.BelongsToSourceField), do: true
   def after?(_), do: false
