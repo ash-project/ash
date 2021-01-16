6ddae6850f0a6d98a88989658d885189b0965ffe
zimt28 <1764689+zimt28@users.noreply.github.com>
2021-01-16 02:41:21 +0100

    improvement: Add trim/allow_empty to string type (#171)

--- lib/ash/type/string.ex
+++ lib/ash/type/string.ex
@@ -11,13 +11,26 @@ defmodule Ash.Type.String do
     match: [
       type: {:custom, __MODULE__, :match, []},
       doc: "Enforces that the string matches a passed in regex"
+    ],
+    trim?: [
+      type: :boolean,
+      doc: "Trims the value.",
+      default: true
+    ],
+    allow_empty?: [
+      type: :boolean,
+      doc: "Sets the value to `nil` if it's empty.",
+      default: false
     ]
   ]
 
   @moduledoc """
-  Stores a string in the database
+  Stores a string in the database.
 
-  A builtin type that can be referenced via `:string`
+  A built-in type that can be referenced via `:string`.
+
+  By default, values are trimmed and empty values are set to `nil`.
+  You can use the `allow_empty?` and `trim?` constraints to change these behaviors.
 
   ### Constraints
 
@@ -34,37 +47,70 @@ defmodule Ash.Type.String do
   def apply_constraints(nil, _), do: :ok
 
   def apply_constraints(value, constraints) do
-    errors =
-      Enum.reduce(constraints, [], fn
-        {:max_length, max_length}, errors ->
-          if String.length(value) > max_length do
-            [[message: "length must be less than or equal to %{max}", max: max_length] | errors]
-          else
-            errors
-          end
-
-        {:min_length, min_length}, errors ->
-          if String.length(value) < min_length do
-            [
-              [message: "length must be greater than or equal to %{min}", min: min_length]
-              | errors
-            ]
-          else
-            errors
-          end
-
-        {:match, regex}, errors ->
-          if String.match?(value, regex) do
-            errors
-          else
-            [{"must match the pattern %{regex}", regex: inspect(regex)} | errors]
-          end
-      end)
+    {value, errors} =
+      return_value(constraints[:allow_empty?], constraints[:trim?], value, constraints)
 
     case errors do
-      [] -> :ok
+      [] -> {:ok, value}
       errors -> {:error, errors}
     end
+  end
+
+  defp return_value(false, true, value, constraints) do
+    trimmed = String.trim(value)
+
+    if trimmed == "" do
+      {nil, []}
+    else
+      {trimmed, validate(trimmed, constraints)}
+    end
+  end
+
+  defp return_value(false, false, value, constraints) do
+    if String.trim(value) == "" do
+      {nil, []}
+    else
+      {value, validate(value, constraints)}
+    end
+  end
+
+  defp return_value(true, true, value, constraints) do
+    trimmed = String.trim(value)
+    {trimmed, validate(trimmed, constraints)}
+  end
+
+  defp return_value(true, false, value, constraints),
+    do: {value, validate(value, constraints)}
+
+  defp validate(value, constraints) do
+    Enum.reduce(constraints, [], fn
+      {:max_length, max_length}, errors ->
+        if String.length(value) > max_length do
+          [[message: "length must be less than or equal to %{max}", max: max_length] | errors]
+        else
+          errors
+        end
+
+      {:min_length, min_length}, errors ->
+        if String.length(value) < min_length do
+          [
+            [message: "length must be greater than or equal to %{min}", min: min_length]
+            | errors
+          ]
+        else
+          errors
+        end
+
+      {:match, regex}, errors ->
+        if String.match?(value, regex) do
+          errors
+        else
+          [{"must match the pattern %{regex}", regex: inspect(regex)} | errors]
+        end
+
+      _, errors ->
+        errors
+    end)
   end
 
   @impl true
---
+++ test/type/string_test.exs
@@ -0,0 +1,118 @@
+defmodule Ash.Test.Type.StringTest do
+  @moduledoc false
+  use ExUnit.Case, async: true
+
+  defmodule Post do
+    @moduledoc false
+    use Ash.Resource, data_layer: Ash.DataLayer.Ets
+
+    ets do
+      private?(true)
+    end
+
+    attributes do
+      uuid_primary_key :id
+
+      # Possible constraint combinations:
+      #
+      # a. [allow_empty?: false, trim?: true] (default)
+      # b. [allow_empty?: false, trim?: false]
+      # c. [allow_empty?: true, trim?: true]
+      # d. [allow_empty?: true, trim?: false]
+      #
+      attribute :string_a, :string
+      attribute :string_b, :string, constraints: [trim?: false]
+      attribute :string_c, :string, constraints: [allow_empty?: true]
+      attribute :string_d, :string, constraints: [allow_empty?: true, trim?: false]
+
+      attribute :string_e, :string, constraints: [min_length: 3, max_length: 6]
+      attribute :string_f, :string, constraints: [min_length: 3, max_length: 6, trim?: false]
+    end
+  end
+
+  defmodule Api do
+    @moduledoc false
+    use Ash.Api
+
+    resources do
+      resource(Post)
+    end
+  end
+
+  import Ash.Changeset
+
+  test "it handles non-empty values" do
+    post =
+      Post
+      |> new(%{
+        string_a: "  foo  ",
+        string_b: "  foo  ",
+        string_c: "  bar  ",
+        string_d: "  bar  "
+      })
+      |> Api.create!()
+
+    assert post.string_a == "foo"
+    assert post.string_b == "  foo  "
+    assert post.string_c == "bar"
+    assert post.string_d == "  bar  "
+  end
+
+  test "it handles empty values" do
+    post =
+      Post
+      |> new(%{
+        string_a: " ",
+        string_b: " ",
+        string_c: " ",
+        string_d: " "
+      })
+      |> Api.create!()
+
+    assert post.string_a == nil
+    assert post.string_b == nil
+    assert post.string_c == ""
+    assert post.string_d == " "
+  end
+
+  test "it handles values with length constraints" do
+    e_allowed_values = ["", "123", "123456", " 123456 "]
+    f_allowed_values = ["", " 2 ", "123456", "  34  "]
+
+    allowed_values = Enum.zip(e_allowed_values, f_allowed_values)
+
+    Enum.each(allowed_values, fn {e_val, f_val} ->
+      Post
+      |> new(%{string_e: e_val, string_f: f_val})
+      |> Api.create!()
+    end)
+  end
+
+  test "it handles too short values with length constraints" do
+    assert_raise(Ash.Error.Invalid, ~r/string_e: length must be greater/, fn ->
+      Post
+      |> new(%{string_e: "   45   "})
+      |> Api.create!()
+    end)
+
+    assert_raise(Ash.Error.Invalid, ~r/string_f: length must be greater/, fn ->
+      Post
+      |> new(%{string_f: "12"})
+      |> Api.create!()
+    end)
+  end
+
+  test "it handles too long values with length constraints" do
+    assert_raise(Ash.Error.Invalid, ~r/string_e: length must be less/, fn ->
+      Post
+      |> new(%{string_e: "1234567"})
+      |> Api.create!()
+    end)
+
+    assert_raise(Ash.Error.Invalid, ~r/string_f: length must be less/, fn ->
+      Post
+      |> new(%{string_f: "   45   "})
+      |> Api.create!()
+    end)
+  end
+end
