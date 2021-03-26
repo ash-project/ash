e4cef22381fb00318be23ffac523f3e4138a07d7
Daniel Kukula <daniel.kuku@gmail.com>
2021-03-26 16:46:54 +0000

    add specs ash.gen.api

--- lib/mix/tasks/ash.gen.api.ex
+++ lib/mix/tasks/ash.gen.api.ex
@@ -1,17 +1,17 @@
 defmodule Mix.Tasks.Ash.Gen.Api do
+  @moduledoc "Initializes new api file lib/api.ex with default content"
+
   alias Mix.Tasks.Ash.Helpers
   @template_path Path.join(:code.priv_dir(:ash), "api.ex.eex")
 
-  @shortdoc """
-  Initializes new api file lib/api.ex with default content
-  """
+  @shortdoc @moduledoc
   @doc """
   Initializes new api file lib/api.ex with default content
 
   run as mix ash.gen api 
   with optional context name
   """
-
+  @spec run(term) :: no_return
   def run([]), do: run(["api"])
 
   def run([file_name | _]) do
@@ -21,6 +21,7 @@ defmodule Mix.Tasks.Ash.Gen.Api do
     Helpers.write_file(file_name, file_content, has_context)
   end
 
+  @spec generate_file(String.t(), boolean()) :: String.t()
   def generate_file(file_name, has_context) do
     EEx.eval_file(@template_path,
       module_name: Helpers.capitalize(file_name),
