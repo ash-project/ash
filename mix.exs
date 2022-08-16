a4f408b66e96bf1e8c1b521cf045469420fa58c7
Zach Daniel <zach@zachdaniel.dev>
2022-08-16 12:46:58 -0400

    fix: don't use local dependency

--- mix.exs
+++ mix.exs
@@ -201,8 +201,7 @@ defmodule Ash.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:spark, "~> 0.1 and >= 0.1.7"},
-      {:spark, path: "../spark"},
+      {:spark, "~> 0.1 and >= 0.1.7"},
       {:ecto, "~> 3.7"},
       {:ets, "~> 0.8.0"},
       {:decimal, "~> 2.0"},
