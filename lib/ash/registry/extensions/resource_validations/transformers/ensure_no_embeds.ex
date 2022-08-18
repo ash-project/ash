a806cac2e2b8dfca098fdc922adfc0fbd1802712
Zach Daniel <zach@zachdaniel.dev>
2022-08-18 15:40:58 -0400

    fix: properly list entries for ensuring no embeds

--- lib/ash/registry/extensions/resource_validations/transformers/ensure_no_embeds.ex
+++ lib/ash/registry/extensions/resource_validations/transformers/ensure_no_embeds.ex
@@ -12,6 +12,7 @@ defmodule Ash.Registry.ResourceValidations.Transformers.EnsureNoEmbeds do
   def transform(dsl) do
     dsl
     |> Transformer.get_entities([:entries])
+    |> Enum.map(& &1.entry)
     |> Enum.filter(&Ash.Resource.Info.embedded?/1)
     |> case do
       [] ->
