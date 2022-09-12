827548f737b1f941ebd43ba0285db6ebd0c27c48
Zach Daniel <zach@zachdaniel.dev>
2022-09-12 13:43:18 -0400

    chore: fix typespec for `aggregate_type/2`

--- lib/ash/resource/info.ex
+++ lib/ash/resource/info.ex
@@ -369,7 +369,7 @@ defmodule Ash.Resource.Info do
   @doc """
   Gets the type of an aggregate for a given resource.
   """
-  @spec aggregate_type(Ash.Resource.t(), Ash.Query.Aggregate.t() | atom) :: Ash.Type.t()
+  @spec aggregate_type(Ash.Resource.t(), Ash.Resource.Aggregate.t() | atom) :: Ash.Type.t()
   def aggregate_type(resource, aggregate) when is_atom(aggregate) do
     aggregate_type(resource, aggregate(resource, aggregate))
   end
