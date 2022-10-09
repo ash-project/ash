ce339165470853ae0f89eef5f2017c25e61a2235
Nelson Bassett <15650992+noteven@users.noreply.github.com>
2022-10-09 03:22:04 -0600

    Merged ValidateAcceptRejectpOverlap transformer into existing DefaultAccept transformer.

--- lib/ash/resource/dsl.ex
+++ lib/ash/resource/dsl.ex
@@ -1222,8 +1222,7 @@ defmodule Ash.Resource.Dsl do
     Ash.Resource.Transformers.ValidateRelationshipAttributes,
     Ash.Resource.Transformers.ValidateEagerIdentities,
     Ash.Resource.Transformers.ValidateAggregatesSupported,
-    Ash.Resource.Transformers.ValidateAccept,
-    Ash.Resource.Transformers.ValidateAcceptRejectOverlap
+    Ash.Resource.Transformers.ValidateAccept
   ]
 
   @moduledoc """
