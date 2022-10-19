49177c34cfef99ebddc77ed35386d22ec5cb3a4a
zimt28 <1764689+zimt28@users.noreply.github.com>
2022-10-19 18:28:06 +0200

    improvement: Add `quarter` value

--- lib/ash/type/duration_name.ex
+++ lib/ash/type/duration_name.ex
@@ -1,5 +1,5 @@
 defmodule Ash.Type.DurationName do
-  @values ~w(year month week day hour minute second millisecond microsecond)a
+  @values ~w(year quarter month week day hour minute second millisecond microsecond)a
   @moduledoc """
   An interval of time, primarily meant to be used in expression functions
 
