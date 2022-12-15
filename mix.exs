41fc04dd85cfc4e0256a204b960524884915d2e5
Zach Daniel <zach@zachdaniel.dev>
2022-12-15 03:57:59 -0500

    improvement: update spark

--- mix.exs
+++ mix.exs
@@ -220,7 +220,7 @@ defmodule Ash.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:spark, "~> 0.2.18"},
+      {:spark, "~> 0.3"},
       {:ecto, "~> 3.7"},
       {:ets, "~> 0.8.0"},
       {:decimal, "~> 2.0"},
