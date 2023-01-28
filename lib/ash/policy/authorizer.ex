2dfff4476a78484d45dbaf776e269b489859ea2a
Rebecca Le <543859+sevenseacat@users.noreply.github.com>
2023-01-29 02:12:23 +0800

    docs: Policy tweaks (#490)

--- lib/ash/policy/authorizer.ex
+++ lib/ash/policy/authorizer.ex
@@ -127,9 +127,9 @@ defmodule Ash.Policy.Authorizer do
     policy always() do
       ...
     end
-
-    See the [policies guide](/documentation/topics/policies.md) for more
     ```
+
+    See the [policies guide](/documentation/topics/policies.md) for more.
     """,
     schema: [
       description: [
@@ -241,7 +241,7 @@ defmodule Ash.Policy.Authorizer do
   For reads, policies can be configured to filter out data that the actor shouldn't see, as opposed to
   resulting in a forbidden error.
 
-  See the [policies guide](/documentation/topics/flows.md) for practical examples.
+  See the [policies guide](/documentation/topics/policies.md) for practical examples.
 
   Policies are solved/managed via a boolean satisfiability solver. To read more about boolean satisfiability,
   see this page: https://en.wikipedia.org/wiki/Boolean_satisfiability_problem. At the end of
