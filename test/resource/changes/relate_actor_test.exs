fd165f163f67649ee34363ae612b2b8e61a388c5
Alan Heywood <alan@softweb.net.au>
2023-04-03 11:00:05 +1000

    improvement: add :field option to relate_actor change
    This allows relating a field of the actor to a resource

--- test/resource/changes/relate_actor_test.exs
+++ test/resource/changes/relate_actor_test.exs
@@ -11,7 +11,29 @@ defmodule Ash.Test.Resource.Changes.RelateActorTest do
     end
 
     actions do
-      create :create
+      defaults [:create, :read]
+
+      create :create_with_account do
+        argument :account, :map
+        change manage_relationship(:account, type: :append_and_remove)
+      end
+    end
+
+    relationships do
+      belongs_to :account, Ash.Test.Resource.Changes.RelateActorTest.Account
+    end
+  end
+
+  defmodule Account do
+    use Ash.Resource,
+      data_layer: Ash.DataLayer.Ets
+
+    attributes do
+      uuid_primary_key :id
+    end
+
+    actions do
+      defaults [:create, :read]
     end
   end
 
@@ -25,7 +47,11 @@ defmodule Ash.Test.Resource.Changes.RelateActorTest do
     end
 
     relationships do
-      belongs_to :author, Author do
+      belongs_to :author, Ash.Test.Resource.Changes.RelateActorTest.Author do
+        allow_nil? true
+      end
+
+      belongs_to :account, Ash.Test.Resource.Changes.RelateActorTest.Account do
         allow_nil? true
       end
     end
@@ -33,6 +59,10 @@ defmodule Ash.Test.Resource.Changes.RelateActorTest do
     actions do
       create :create_with_actor do
         change relate_actor(:author)
+      end
+
+      create :create_with_actor_field do
+        change relate_actor(:account, field: :account)
       end
 
       create :create_possibly_without_actor do
@@ -46,6 +76,7 @@ defmodule Ash.Test.Resource.Changes.RelateActorTest do
     use Ash.Registry
 
     entries do
+      entry Account
       entry Author
       entry Post
     end
@@ -80,6 +111,41 @@ defmodule Ash.Test.Resource.Changes.RelateActorTest do
       |> Api.create()
 
     assert changeset.errors |> Enum.count() == 1
+  end
+
+  test "relate_actor change with field" do
+    account =
+      Account
+      |> Ash.Changeset.for_create(:create, %{})
+      |> Api.create!()
+
+    actor =
+      Author
+      |> Ash.Changeset.for_create(:create_with_account, %{account: account})
+      |> Api.create!()
+
+    post =
+      Post
+      |> Ash.Changeset.for_create(:create_with_actor_field, %{text: "foo"}, actor: actor)
+      |> Api.create!()
+
+    assert post.account_id == account.id
+  end
+
+  @tag :focus
+  test "relate_actor change with field when field is nil" do
+    actor =
+      Author
+      |> Ash.Changeset.for_create(:create)
+      |> Api.create!()
+      |> Api.load!(:account)
+
+    post =
+      Post
+      |> Ash.Changeset.for_create(:create_with_actor_field, %{text: "foo"}, actor: actor)
+      |> Api.create!()
+
+    assert post.account_id == nil
   end
 
   test "relate_actor change with `allow_nil?: true` allows both nil and an actor" do
