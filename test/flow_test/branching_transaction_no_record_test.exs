e6102c372d7d5458bd174bf0266ea33b10a8eb2b
Tommaso Patrizi <tommasop@gmail.com>
2023-04-04 10:23:13 +0200

    tests for custom steps returning nil even when there is a right result

---
+++ test/flow_test/branching_transaction_no_record_test.exs
@@ -0,0 +1,55 @@
+defmodule Ash.FlowTest.BranchingTransactionNoRecordTest do
+  @moduledoc false
+  use ExUnit.Case, async: false
+  alias Ash.Test.Flow.{Api, Org, User}
+
+  alias Ash.Test.Flow.Flows.SignUpUser
+
+  setup do
+    ExUnit.CaptureLog.capture_log(fn ->
+      Ash.DataLayer.Mnesia.start(Ash.Test.Flow.Api)
+    end)
+
+    on_exit(fn ->
+      ExUnit.CaptureLog.capture_log(fn ->
+        :mnesia.stop()
+        :mnesia.delete_schema([node()])
+      end)
+    end)
+
+    org =
+      Org
+      |> Ash.Changeset.for_create(:create, %{name: "Org 1"})
+      |> Api.create!()
+
+    SignUpUser.run!(org.name, "Bruce", "Wayne")
+    :ok
+  end
+
+  test "if user exists does not create user" do
+    assert %Ash.Flow.Result{result: %{user_not_found: nil, get_user: user}} =
+             Ash.Test.Flow.Flows.BranchingTransactionNoRecord.run!("Bruce")
+    assert user.first_name == "Bruce"
+  end
+
+  test "if user does not exist create new org and user" do
+    user =
+      User
+      |> Ash.Query.for_read(:by_name, %{name: "Bat"})
+      |> Api.read!()
+
+    assert user == []
+
+    %Ash.Flow.Result{result: %{user_not_found: user, get_user: nil}} = Ash.Test.Flow.Flows.BranchingTransactionNoRecord.run!("Bat")
+    IO.inspect(user)
+
+    assert user.create_user.first_name == "Bat"
+
+    users =
+      User
+      |> Ash.Query.for_read(:index)
+      |> Api.read!()
+
+    assert length(users) == 2
+  end
+end
---
+++ test/support/flow/flows/branching_transaction_no_record.ex
@@ -0,0 +1,41 @@
+defmodule Ash.Test.Flow.Flows.BranchingTransactionNoRecord do
+  @moduledoc false
+  use Ash.Flow
+
+  flow do
+    api Ash.Test.Flow.Api
+
+    argument :name, :string do
+      allow_nil? false
+    end
+
+    returns [:user_not_found, :get_user]
+  end
+
+  steps do
+    read :get_user, Ash.Test.Flow.User, :by_name do
+      get? true
+      not_found_error? false
+
+      input(%{
+        name: arg(:name)
+      })
+    end
+
+    branch :user_not_found, expr(is_nil(^result(:get_user))) do
+      transaction :create_org_and_user, Ash.Test.Flow.User do
+        create :create_org, Ash.Test.Flow.Org, :create do
+          input %{name: "Bat Company"}
+        end
+
+        create :create_user, Ash.Test.Flow.User, :create do
+          input %{
+            first_name: arg(:name),
+            last_name: "Man",
+            org: path(result(:create_org), [:id])
+          }
+        end
+      end
+    end
+  end
+end
