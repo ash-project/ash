8076598eb22ce00346a345f982d6f03a046e8b7f
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2023-04-05 10:25:39 +0200

    add constraints to maps

--- lib/ash/type/map.ex
+++ lib/ash/type/map.ex
@@ -1,12 +1,50 @@
 defmodule Ash.Type.Map do
+  @constraints [
+    fields: [
+      type: {:custom, __MODULE__, :field_types, []},
+      doc: """
+      The types of the fields in the map
+
+      For example:
+
+          fields:  [
+            amount: [
+              type: :integer,
+              constraints: [
+                max: 10
+              ]
+            ],
+            currency: [
+              type: :string,
+              allow_nil?: false,
+              constraints: [
+                max_length: 3
+              ]
+            ]
+          ]
+
+      allow_nil? is true by default
+      """
+    ]
+  ]
+
+  def field_types(value) do
+    {:ok, value}
+  end
+
   @moduledoc """
   Represents a map stored in the database.
 
   In postgres, for example, this represents binary encoded json
 
   A builtin type that can be referenced via `:map`
+
+  #{Spark.OptionsHelpers.docs(@constraints)}
   """
   use Ash.Type
+
+  @impl true
+  def constraints, do: @constraints
 
   @impl true
   def storage_type, do: :map
@@ -16,17 +54,17 @@ defmodule Ash.Type.Map do
 
   def cast_input(nil, _), do: {:ok, nil}
 
-  def cast_input(value, _) when is_binary(value) do
+  def cast_input(value, constraints) when is_binary(value) do
     case Jason.decode(value) do
       {:ok, value} ->
-        {:ok, value}
+        check_constraints(value, constraints)
 
       _ ->
         :error
     end
   end
 
-  def cast_input(value, _) when is_map(value), do: {:ok, value}
+  def cast_input(value, constraints) when is_map(value), do: check_constraints(value, constraints)
   def cast_input(_, _), do: :error
 
   @impl true
@@ -38,4 +76,44 @@ defmodule Ash.Type.Map do
   def dump_to_native(nil, _), do: {:ok, nil}
   def dump_to_native(value, _) when is_map(value), do: {:ok, value}
   def dump_to_native(_, _), do: :error
+
+  defp check_constraints(value, constraints) do
+    Enum.reduce(constraints, {:ok, value}, fn
+      {:fields, fields}, {:ok, value} ->
+        check_fields(value, fields)
+
+      {_field, _}, {:error, errors} ->
+        {:error, errors}
+    end)
+  end
+
+  defp check_fields(value, fields) do
+    Enum.reduce(fields, {:ok, value}, fn
+      {field, field_constraints}, {:ok, value} ->
+        case value[field] do
+          nil ->
+            if field_constraints[:allow_nil?] == false do
+              {:error, [[message: "must not be nil", field: field]]}
+            else
+              {:ok, value}
+            end
+
+          field_value ->
+            case Ash.Type.cast_input(
+                   field_constraints[:type],
+                   field_value,
+                   field_constraints[:constraints]
+                 ) do
+              {:ok, field_value} ->
+                {:ok, Map.put(value, field, field_value)}
+
+              {:error, errors} ->
+                {:error, errors}
+            end
+        end
+
+      {_field, _}, {:error, errors} ->
+        {:error, errors}
+    end)
+  end
 end
---
+++ test/type/map_test.exs
@@ -0,0 +1,105 @@
+defmodule Type.MapTest do
+  use ExUnit.Case, async: true
+
+  import Ash.Changeset
+
+  defmodule Post do
+    @moduledoc false
+    use Ash.Resource, data_layer: Ash.DataLayer.Ets
+
+    ets do
+      private?(true)
+    end
+
+    actions do
+      defaults [:create, :read, :update, :destroy]
+    end
+
+    attributes do
+      uuid_primary_key :id
+
+      attribute :metadata, :map do
+        constraints fields: [
+                      foo: [type: :string, allow_nil?: false],
+                      bar: [type: :integer, constraints: [min: 0]]
+                    ]
+      end
+    end
+  end
+
+  defmodule Registry do
+    @moduledoc false
+    use Ash.Registry
+
+    entries do
+      entry Post
+    end
+  end
+
+  defmodule Api do
+    @moduledoc false
+    use Ash.Api
+
+    resources do
+      registry Registry
+    end
+  end
+
+  test "it handles valid maps" do
+    changeset =
+      Post
+      |> for_create(:create, %{
+        metadata: %{
+          foo: "bar",
+          bar: 1
+        }
+      })
+
+    assert changeset.valid?
+  end
+
+  test "allow_nil? is true by default" do
+    changeset =
+      Post
+      |> for_create(:create, %{
+        metadata: %{
+          foo: "bar",
+          bar: nil
+        }
+      })
+
+    assert changeset.valid?
+  end
+
+  test "keys that can be nil don't need to be there" do
+    changeset =
+      Post
+      |> for_create(:create, %{
+        metadata: %{
+          foo: "bar"
+        }
+      })
+
+    assert changeset.valid?
+  end
+
+  test "keys that can not be nil need to be there" do
+    changeset =
+      Post
+      |> for_create(:create, %{
+        metadata: %{bar: 1}
+      })
+
+    refute changeset.valid?
+  end
+
+  test "constraints of field types are checked" do
+    changeset =
+      Post
+      |> for_create(:create, %{
+        metadata: %{bar: -1}
+      })
+
+    refute changeset.valid?
+  end
+end
