b5e0cbb3cc91db77f64ddf31eb09a1806c9263dc
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2023-04-07 15:53:41 +0200

    improvement: add constraints to maps (#556)

--- lib/ash/type/map.ex
+++ lib/ash/type/map.ex
@@ -1,12 +1,71 @@
 defmodule Ash.Type.Map do
+  @constraints [
+    fields: [
+      type: :keyword_list,
+      keys: [
+        *: [
+          type: :keyword_list,
+          keys: [
+            type: [
+              type: Ash.OptionsHelpers.ash_type(),
+              required: true
+            ],
+            allow_nil?: [
+              type: :boolean,
+              default: true
+            ],
+            constraints: [
+              type: :keyword_list,
+              default: []
+            ]
+          ]
+        ]
+      ],
+      doc: """
+      The types of the fields in the map, and their constraints.
+
+      If constraints are specified, only those fields will be in the casted map.
+
+      For example:
+
+          fields:  [
+            amount: [
+              type: :integer,
+              constraints: [
+                max: 10
+              ]
+            ],
+            currency: [
+              type: :string,
+              allow_nil?: false,
+              constraints: [
+                max_length: 3
+              ]
+            ]
+          ]
+
+      allow_nil? is true by default
+      """
+    ]
+  ]
+
+  def field_types(value) do
+    {:ok, value}
+  end
+
   @moduledoc """
   Represents a map stored in the database.
 
   In postgres, for example, this represents binary encoded json
 
   A builtin type that can be referenced via `:map`
+
+  #{Spark.OptionsHelpers.docs(@constraints)}
   """
   use Ash.Type
+
+  @impl true
+  def constraints, do: @constraints
 
   @impl true
   def storage_type, do: :map
@@ -16,10 +75,10 @@ defmodule Ash.Type.Map do
 
   def cast_input(nil, _), do: {:ok, nil}
 
-  def cast_input(value, _) when is_binary(value) do
+  def cast_input(value, constraints) when is_binary(value) do
     case Jason.decode(value) do
       {:ok, value} ->
-        {:ok, value}
+        cast_input(value, constraints)
 
       _ ->
         :error
@@ -31,11 +90,84 @@ defmodule Ash.Type.Map do
 
   @impl true
   def cast_stored(nil, _), do: {:ok, nil}
+
   def cast_stored(value, _) when is_map(value), do: {:ok, value}
+
   def cast_stored(_, _), do: :error
 
   @impl true
   def dump_to_native(nil, _), do: {:ok, nil}
   def dump_to_native(value, _) when is_map(value), do: {:ok, value}
   def dump_to_native(_, _), do: :error
+
+  @impl true
+  def apply_constraints(value, constraints) do
+    Enum.reduce(constraints, {:ok, value}, fn
+      {:fields, fields}, {:ok, value} ->
+        check_fields(value, fields)
+
+      {_, _}, {:error, errors} ->
+        {:error, errors}
+    end)
+  end
+
+  defp check_fields(value, fields) do
+    Enum.reduce(fields, {:ok, %{}}, fn
+      {field, field_constraints}, {:ok, checked_value} ->
+        case fetch_field(value, field) do
+          {:ok, field_value} ->
+            check_field(checked_value, field, field_value, field_constraints)
+
+          :error ->
+            if field_constraints[:allow_nil?] == false do
+              {:error, [[message: "field must be present", field: field]]}
+            else
+              {:ok, checked_value}
+            end
+        end
+
+      {_, _}, {:error, errors} ->
+        {:error, errors}
+    end)
+  end
+
+  defp check_field(result, field, field_value, field_constraints) do
+    case Ash.Type.cast_input(
+           field_constraints[:type],
+           field_value,
+           field_constraints[:type]
+         ) do
+      {:ok, field_value} ->
+        case Ash.Type.apply_constraints(
+               field_constraints[:type],
+               field_value,
+               field_constraints[:constraints]
+             ) do
+          {:ok, nil} ->
+            if field_constraints[:allow_nil?] == false do
+              {:error, [[message: "value must not be nil", field: field]]}
+            else
+              {:ok, Map.put(result, field, nil)}
+            end
+
+          {:ok, field_value} ->
+            {:ok, Map.put(result, field, field_value)}
+
+          {:error, errors} ->
+            {:error, Enum.map(errors, fn error -> [field: field, message: error[:message]] end)}
+        end
+
+      :error ->
+        {:error, [[message: "invalid value", field: field]]}
+    end
+  end
+
+  defp fetch_field(map, atom) when is_atom(atom) do
+    case Map.fetch(map, atom) do
+      {:ok, value} -> {:ok, value}
+      :error -> fetch_field(map, to_string(atom))
+    end
+  end
+
+  defp fetch_field(map, key), do: Map.fetch(map, key)
 end
---
+++ test/type/map_test.exs
@@ -0,0 +1,228 @@
+defmodule Type.MapTest do
+  use ExUnit.Case, async: true
+
+  import Ash.Changeset
+
+  defmodule Post do
+    @moduledoc false
+    use Ash.Resource, data_layer: Ash.DataLayer.Ets
+
+    ets do
+      private?(true)
+    end
+
+    actions do
+      defaults [:create, :read, :update, :destroy]
+    end
+
+    attributes do
+      uuid_primary_key :id
+
+      attribute :metadata, :map do
+        constraints fields: [
+                      foo: [type: :string, allow_nil?: false],
+                      bar: [type: :integer, constraints: [min: 0]]
+                    ]
+      end
+
+      attribute :otherdata, :map
+    end
+  end
+
+  defmodule Registry do
+    @moduledoc false
+    use Ash.Registry
+
+    entries do
+      entry Post
+    end
+  end
+
+  defmodule Api do
+    @moduledoc false
+    use Ash.Api
+
+    resources do
+      registry Registry
+    end
+  end
+
+  test "it handles valid maps" do
+    changeset =
+      Post
+      |> for_create(:create, %{
+        metadata: %{
+          foo: "bar",
+          bar: 1
+        }
+      })
+
+    assert changeset.valid?
+  end
+
+  test "allow_nil? is true by default" do
+    changeset =
+      Post
+      |> for_create(:create, %{
+        metadata: %{
+          foo: "bar",
+          bar: "2"
+        }
+      })
+
+    assert changeset.valid?
+
+    assert changeset.attributes == %{
+             metadata: %{foo: "bar", bar: 2}
+           }
+  end
+
+  test "cast result has only atom keys" do
+    changeset =
+      Post
+      |> for_create(:create, %{
+        metadata: %{
+          "bar" => nil,
+          foo: "bar"
+        }
+      })
+
+    assert changeset.valid?
+
+    assert changeset.attributes == %{
+             metadata: %{foo: "bar", bar: nil}
+           }
+  end
+
+  test "keys that can be nil don't need to be there" do
+    changeset =
+      Post
+      |> for_create(:create, %{
+        metadata: %{
+          foo: "bar"
+        }
+      })
+
+    assert changeset.valid?
+  end
+
+  test "keys that can not be nil need to be there" do
+    changeset =
+      Post
+      |> for_create(:create, %{
+        metadata: %{bar: 1}
+      })
+
+    refute changeset.valid?
+
+    assert [
+             %Ash.Error.Changes.InvalidAttribute{
+               field: :metadata,
+               message: "at field foo field must be present",
+               private_vars: nil,
+               value: %{bar: 1},
+               changeset: nil,
+               query: nil,
+               error_context: [],
+               vars: [field: :metadata, message: "at field foo field must be present"],
+               path: []
+             }
+           ] = changeset.errors
+  end
+
+  test "constraints of field types are checked" do
+    changeset =
+      Post
+      |> for_create(:create, %{
+        metadata: %{foo: "hello", bar: -1}
+      })
+
+    refute changeset.valid?
+
+    assert [
+             %Ash.Error.Changes.InvalidAttribute{
+               field: :metadata,
+               message: "at field bar must be more than or equal to %{min}",
+               private_vars: nil,
+               value: %{bar: -1, foo: "hello"},
+               changeset: nil,
+               query: nil,
+               error_context: [],
+               vars: [
+                 field: :metadata,
+                 message: "at field bar must be more than or equal to %{min}"
+               ],
+               path: []
+             }
+           ] = changeset.errors
+  end
+
+  test "extra fields are removed" do
+    changeset =
+      Post
+      |> for_create(
+        :create,
+        %{
+          metadata: %{
+            "foo" => "bar",
+            extra: "field"
+          }
+        }
+      )
+
+    assert changeset.valid?
+
+    assert changeset.attributes == %{
+             metadata: %{foo: "bar"}
+           }
+  end
+
+  test "values are casted before checked" do
+    changeset =
+      Post
+      |> for_create(
+        :create,
+        %{
+          metadata: %{
+            "foo" => "",
+            bar: "2"
+          }
+        }
+      )
+
+    refute changeset.valid?
+
+    assert [
+             %Ash.Error.Changes.InvalidAttribute{
+               field: :metadata,
+               message: "at field foo value must not be nil",
+               private_vars: nil,
+               value: %{:bar => "2", "foo" => ""},
+               changeset: nil,
+               query: nil,
+               error_context: [],
+               vars: [field: :metadata, message: "at field foo value must not be nil"],
+               path: []
+             }
+           ] = changeset.errors
+  end
+
+  test "maps without constraints are left as is" do
+    changeset =
+      Post
+      |> for_create(
+        :create,
+        %{
+          otherdata: %{
+            extra: "field"
+          }
+        }
+      )
+
+    assert changeset.valid?
+
+    assert changeset.attributes == %{
+             otherdata: %{extra: "field"}
+           }
+  end
+end
