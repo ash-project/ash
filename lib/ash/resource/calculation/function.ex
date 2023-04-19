5005d57b1db3522a09accc6f10418ac230fe2c40
Zach Daniel <zach@zachdaniel.dev>
2023-04-18 20:22:17 -0400

    fix: add the spark function version of calculations

---
+++ lib/ash/resource/calculation/function.ex
@@ -0,0 +1,22 @@
+defmodule Ash.Calculation.Function do
+  @moduledoc false
+  use Ash.Calculation
+
+  def calculate(results, [fun: fun], context) do
+    Enum.reduce_while(results, {:ok, []}, fn result, {:ok, acc} ->
+      case apply_fun(fun, result, context) do
+        {:ok, result} -> {:cont, {:ok, [result | acc]}}
+        {:error, error} -> {:halt, {:error, error}}
+        result -> {:cont, {:ok, [result | acc]}}
+      end
+    end)
+  end
+
+  defp apply_fun({m, f, a}, result, context) do
+    apply(m, f, [result, context | a])
+  end
+
+  defp apply_fun(fun, result, context) do
+    fun.(result, context)
+  end
+end
