4e5913588ae61cd1cfdb9475365edf172c229439
Zach Daniel <zach@zachdaniel.dev>
2023-04-21 10:44:22 -0600

    fix: more fixes to optimized boolean filter building

--- lib/ash/query/boolean_expression.ex
+++ lib/ash/query/boolean_expression.ex
@@ -1,6 +1,6 @@
 defmodule Ash.Query.BooleanExpression do
   @moduledoc "Represents a boolean expression"
-  @dialyzer {:nowarn_function, optimized_new: 4}
+  @dialyzer {:nowarn_function, optimized_new: 3}
 
   defstruct [:op, :left, :right]
 
@@ -22,14 +22,13 @@ defmodule Ash.Query.BooleanExpression do
   # statements where possible, which helps with authorization logic that leverages the query
   # For example, `x in [1, 2] or x == 3` becomes `x in [1, 2, 3]`, and `x in [1, 2, 3] and x != 1`
   # becomes `x in [2, 3]`
-  def optimized_new(op, left, right, current_op \\ :and)
-  def optimized_new(_, nil, nil, _), do: nil
-  def optimized_new(:and, false, _, _), do: false
-  def optimized_new(:and, _, false, _), do: false
-  def optimized_new(:or, true, _, _), do: true
-  def optimized_new(:or, _, true, _), do: true
-  def optimized_new(_, nil, right, _), do: right
-  def optimized_new(_, left, nil, _), do: left
+  def optimized_new(_, nil, nil), do: nil
+  def optimized_new(:and, false, _), do: false
+  def optimized_new(:and, _, false), do: false
+  def optimized_new(:or, true, _), do: true
+  def optimized_new(:or, _, true), do: true
+  def optimized_new(_, nil, right), do: right
+  def optimized_new(_, left, nil), do: left
 
   def optimized_new(
         op,
@@ -38,49 +37,47 @@ defmodule Ash.Query.BooleanExpression do
           op: op,
           left: left,
           right: right
-        },
-        op
+        }
       ) do
-    optimized_new(op, optimized_new(op, left_expr, left, op), right, op)
+    optimized_new(op, optimized_new(op, left_expr, left), right)
   end
 
-  def optimized_new(op, %__MODULE__{} = left, %__MODULE__{} = right, _) do
+  def optimized_new(op, %__MODULE__{} = left, %__MODULE__{} = right) do
     do_new(op, left, right)
   end
 
-  def optimized_new(op, left, %__MODULE__{} = right, current_op) do
-    optimized_new(op, right, left, current_op)
+  def optimized_new(op, left, %__MODULE__{} = right) do
+    optimized_new(op, right, left)
   end
 
-  def optimized_new(op, %In{} = left, %NotEq{} = right, current_op) do
-    optimized_new(op, right, left, current_op)
+  def optimized_new(op, %In{} = left, %NotEq{} = right) do
+    optimized_new(op, right, left)
   end
 
-  def optimized_new(op, %In{} = left, %Eq{} = right, current_op) do
-    optimized_new(op, right, left, current_op)
+  def optimized_new(op, %In{} = left, %Eq{} = right) do
+    optimized_new(op, right, left)
   end
 
-  def optimized_new(op, %Eq{right: %Ref{}} = left, right, _) do
+  def optimized_new(op, %Eq{right: %Ref{}} = left, right) do
     do_new(op, left, right)
   end
 
-  def optimized_new(op, left, %Eq{right: %Ref{}} = right, _) do
+  def optimized_new(op, left, %Eq{right: %Ref{}} = right) do
     do_new(op, left, right)
   end
 
-  def optimized_new(op, %NotEq{right: %Ref{}} = left, right, _) do
+  def optimized_new(op, %NotEq{right: %Ref{}} = left, right) do
     do_new(op, left, right)
   end
 
-  def optimized_new(op, left, %NotEq{right: %Ref{}} = right, _) do
+  def optimized_new(op, left, %NotEq{right: %Ref{}} = right) do
     do_new(op, left, right)
   end
 
   def optimized_new(
         :or,
         %Eq{left: left, right: value} = left_op,
-        %In{left: left, right: %{__struct__: MapSet} = mapset} = right,
-        _
+        %In{left: left, right: %{__struct__: MapSet} = mapset} = right
       ) do
     if can_optimize?(value) do
       %{right | right: MapSet.put(mapset, value)}
@@ -90,34 +87,9 @@ defmodule Ash.Query.BooleanExpression do
   end
 
   def optimized_new(
-        :or,
-        %NotEq{left: left, right: value} = left_op,
-        %In{left: left, right: %{__struct__: MapSet} = mapset} = right,
-        _
-      ) do
-    if can_optimize?(value) do
-      without = MapSet.delete(mapset, value)
-
-      case MapSet.size(without) do
-        0 ->
-          do_new(:or, left_op, right)
-
-        1 ->
-          %Eq{left: left, right: Enum.at(without, 0)}
-
-        _ ->
-          %{right | right: without}
-      end
-    else
-      do_new(:or, left_op, right)
-    end
-  end
-
-  def optimized_new(
         :and,
         %Eq{left: left, right: value} = left_expr,
-        %In{left: left, right: %{__struct__: MapSet} = mapset} = right,
-        _
+        %In{left: left, right: %{__struct__: MapSet} = mapset} = right
       ) do
     if can_optimize?(value) do
       if MapSet.member?(mapset, value) do
@@ -133,8 +105,7 @@ defmodule Ash.Query.BooleanExpression do
   def optimized_new(
         :and,
         %NotEq{left: left, right: value} = left_op,
-        %In{left: left, right: %{__struct__: MapSet} = mapset} = right_expr,
-        _
+        %In{left: left, right: %{__struct__: MapSet} = mapset} = right_expr
       ) do
     if can_optimize?(value) do
       %{right_expr | right: MapSet.delete(mapset, value)}
@@ -146,17 +117,15 @@ defmodule Ash.Query.BooleanExpression do
   def optimized_new(
         op,
         %NotEq{} = left,
-        %Eq{} = right,
-        current_op
+        %Eq{} = right
       ) do
-    optimized_new(op, right, left, current_op)
+    optimized_new(op, right, left)
   end
 
   def optimized_new(
         :or,
         %Eq{left: left, right: left_value} = left_expr,
-        %Eq{left: left, right: right_value} = right_expr,
-        _
+        %Eq{left: left, right: right_value} = right_expr
       ) do
     if can_optimize?(left_value) && can_optimize?(right_value) do
       %In{left: left, right: MapSet.new([left_value, right_value])}
@@ -168,8 +137,7 @@ defmodule Ash.Query.BooleanExpression do
   def optimized_new(
         :and,
         %Eq{left: left, right: left_value} = left_expr,
-        %Eq{left: left, right: right_value} = right_expr,
-        _
+        %Eq{left: left, right: right_value} = right_expr
       ) do
     if can_optimize?(left_value) && can_optimize?(right_value) && left_value == right_value do
       left_expr
@@ -181,8 +149,7 @@ defmodule Ash.Query.BooleanExpression do
   def optimized_new(
         :and,
         %NotEq{left: left, right: left_value} = left_expr,
-        %NotEq{left: left, right: right_value} = right_expr,
-        _
+        %NotEq{left: left, right: right_value} = right_expr
       ) do
     if can_optimize?(left_value) && can_optimize?(right_value) && left_value == right_value do
       left_value
@@ -194,8 +161,7 @@ defmodule Ash.Query.BooleanExpression do
   def optimized_new(
         :or,
         %In{left: left, right: %{__struct__: MapSet} = left_values},
-        %In{left: left, right: %{__struct__: MapSet} = right_values} = right_expr,
-        _
+        %In{left: left, right: %{__struct__: MapSet} = right_values} = right_expr
       ) do
     %{right_expr | right: MapSet.union(left_values, right_values)}
   end
@@ -203,14 +169,13 @@ defmodule Ash.Query.BooleanExpression do
   def optimized_new(
         :and,
         %In{left: left, right: left_values} = left_expr,
-        %In{left: left, right: right_values} = right_expr,
-        _
+        %In{left: left, right: right_values} = right_expr
       ) do
     if can_optimize?(left_values) && can_optimize?(right_values) do
       intersection = MapSet.intersection(left_values, right_values)
 
       case MapSet.size(intersection) do
-        0 -> do_new(:and, left_expr, right_expr)
+        0 -> false
         1 -> %Eq{left: left, right: Enum.at(intersection, 0)}
         _ -> %{right_expr | right: intersection}
       end
@@ -220,45 +185,44 @@ defmodule Ash.Query.BooleanExpression do
   end
 
   def optimized_new(
-        :and,
-        %__MODULE__{op: :and, left: left, right: right} = left_expr,
-        right_expr,
-        op
+        :or,
+        %__MODULE__{left: left, right: right} = left_expr,
+        right_expr
       ) do
     case right_expr do
       %In{} = in_op ->
-        with {:left, nil} <- {:left, Ash.Filter.find(left, &simplify?(&1, in_op), false)},
-             {:right, nil} <- {:right, Ash.Filter.find(right, &simplify?(&1, in_op), false)} do
-          do_new(op, left_expr, in_op)
+        with {:left, nil} <- {:left, Ash.Filter.find(left, &simplify?(&1, in_op))},
+             {:right, nil} <- {:right, Ash.Filter.find(right, &simplify?(&1, in_op))} do
+          do_new(:or, left_expr, in_op)
         else
           {:left, _} ->
-            %{left_expr | left: optimized_new(op, left, in_op)}
+            %{left_expr | left: optimized_new(:or, left, in_op)}
 
           {:right, _} ->
-            %{left_expr | right: optimized_new(op, right, in_op)}
+            %{left_expr | right: optimized_new(:or, right, in_op)}
         end
 
       %Eq{} = eq_op ->
         with {:left, nil} <-
-               {:left, Ash.Filter.find(left, &simplify?(&1, eq_op), false)},
+               {:left, Ash.Filter.find(left, &simplify?(&1, eq_op))},
              {:right, nil} <-
-               {:right, Ash.Filter.find(right, &simplify?(&1, eq_op), false)} do
-          do_new(op, left_expr, eq_op)
+               {:right, Ash.Filter.find(right, &simplify?(&1, eq_op))} do
+          do_new(:or, left_expr, eq_op)
         else
           {:left, _} ->
-            %{left_expr | left: optimized_new(op, left, eq_op)}
+            %{left_expr | left: optimized_new(:or, left, eq_op)}
 
           {:right, _} ->
-            %{left_expr | right: optimized_new(op, right, eq_op)}
+            %{left_expr | right: optimized_new(:or, right, eq_op)}
         end
 
       _ ->
-        do_new(op, left_expr, right_expr)
+        do_new(:or, left_expr, right_expr)
     end
   end
 
-  def optimized_new(op, left, right, _) do
-    # TODO: more optimization passes!
+  def optimized_new(op, left, right) do
+    # TODO: more optimization passes
     # Remove predicates that are on both sides of an `and`
     # if a predicate is on both sides of an `or`, lift it to an `and`
     do_new(op, left, right)
@@ -319,11 +283,9 @@ defmodule Ash.Query.BooleanExpression do
     Enum.all?(mapset, &can_optimize?/1)
   end
 
-  defp can_optimize?(value) when is_integer(value) or is_binary(value) do
-    true
+  defp can_optimize?(value) do
+    !Ash.Filter.TemplateHelpers.expr?(value)
   end
-
-  defp can_optimize?(_), do: false
 
   defp do_new(op, left, right) do
     if left == right do
