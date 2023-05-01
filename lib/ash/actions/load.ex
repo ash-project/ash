f05527e4594989e4227d3878c4c45eada1d352bf
Zach Daniel <zach@zachdaniel.dev>
2023-05-01 01:05:26 -0400

    fix: handle map relationship data in lateral join

--- lib/ash/actions/load.ex
+++ lib/ash/actions/load.ex
@@ -1427,7 +1427,15 @@ defmodule Ash.Actions.Load do
        ) do
     {offset, limit} = offset_and_limit(base_query)
 
-    source_data = get_in(data, source_data_path)
+    source_data = 
+      case get_in(data, source_data_path) do
+        value when is_map(value) ->
+          value
+          |> Map.values()
+          |> Enum.flat_map(&List.wrap/1)
+        value ->
+          value
+      end
 
     lazy_load_or(
       source_data,
