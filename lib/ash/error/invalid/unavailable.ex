66ea912f8c2cbd55f9b49214c744944fd10d74c6
Zach Daniel <zach@zachdaniel.dev>
2023-05-17 13:15:24 -0400

    improvement: add unavailable error

---
+++ lib/ash/error/invalid/unavailable.ex
@@ -0,0 +1,34 @@
+defmodule Ash.Error.Invalid.Unavailable do
+  @moduledoc """
+  Used when a given resource is unavailable.
+
+  This might happen due to locking at the data layer, or something
+  you implement yourself.
+  """
+  use Ash.Error.Exception
+
+  def_ash_error([:resource, :source, :reason], class: :invalid)
+
+  defimpl Ash.ErrorKind do
+    def id(_), do: Ash.UUID.generate()
+
+    def code(_), do: "unavailable"
+
+    @spec message(%Ash.Error.Invalid.Unavailable{}) :: <<_::64, _::_*8>>
+    def message(%{resource: resource, source: source, reason: reason}) when not is_nil(source) do
+      """
+      #{inspect(resource)} - the resource is not available#{with_reason(reason)}.
+
+      In:
+
+      #{source}
+      """
+    end
+
+    def message(%{resource: resource, reason: reason}),
+      do: "#{inspect(resource)} - the resource is not available#{with_reason(reason)}"
+
+    defp with_reason(nil), do: ""
+    defp with_reason(reason), do: ": #{reason}"
+  end
+end
