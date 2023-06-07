87045f3984b4dc19a6fee7ac6238491710bce5cc
Adam Harris <32979919+oco-adam@users.noreply.github.com>
2023-06-07 16:13:08 +0200

    chore: add failing test to illustrate calculation issue

--- test/resource/calculations_test.exs
+++ test/resource/calculations_test.exs
@@ -2,6 +2,8 @@ defmodule Ash.Test.Resource.CalculationsTest do
   @moduledoc false
   use ExUnit.Case, async: true
 
+  alias Ash.Test.Support.PolicySimple.Api
+  alias Ash.Test.Support.PolicySimple.Post
   alias Ash.Resource.Calculation
 
   defmacrop defposts(do: body) do
@@ -67,6 +69,207 @@ defmodule Ash.Test.Resource.CalculationsTest do
       assert [
                %Ash.Resource.Calculation{description: "require one of name/contents"}
              ] = Ash.Resource.Info.calculations(Post)
+    end
+  end
+
+  describe "relationships" do
+    test "calculations can access attributes of parent" do
+      defmodule Post do
+        @moduledoc false
+        use Ash.Resource, data_layer: Ash.DataLayer.Ets
+
+        attributes do
+          uuid_primary_key :id
+
+          attribute :name, :string
+          attribute :contents, :string
+        end
+
+        actions do
+          defaults [:read, :update, :destroy, :create]
+        end
+      end
+
+      defmodule PostName do
+        @moduledoc """
+        Calculates the name of the post, from the child comment.
+        """
+        use Ash.Calculation
+
+        @impl true
+        def load(_query, _opts, _context), do: [:post]
+
+        @impl true
+        def calculate(records, _opts, _) do
+          Enum.map(records, fn comment ->
+            post = comment.post
+            post.name
+          end)
+        end
+      end
+
+      defmodule Comment do
+        @moduledoc false
+        use Ash.Resource, data_layer: Ash.DataLayer.Ets
+
+        attributes do
+          uuid_primary_key :id
+
+          attribute :post_id, :uuid do
+            allow_nil?(false)
+          end
+        end
+
+        actions do
+          defaults [:read, :update, :destroy, :create]
+        end
+
+        relationships do
+          belongs_to :post, Post
+        end
+
+        calculations do
+          calculate(:post_name, :string, PostName)
+        end
+      end
+
+      defmodule Registry do
+        @moduledoc false
+        use Ash.Registry
+
+        entries do
+          entry(Post)
+          entry(Comment)
+        end
+      end
+
+      defmodule MyApi do
+        @moduledoc false
+        use Ash.Api
+
+        resources do
+          registry Registry
+        end
+      end
+
+      post =
+        Post
+        |> Ash.Changeset.for_create(:create, %{name: "Post 1", contents: "Contents 1"})
+        |> MyApi.create!()
+
+      comment =
+        Comment
+        |> Ash.Changeset.for_create(:create, %{post_id: post.id})
+        |> MyApi.create!()
+
+      # assert true == true
+      comment_with_post_name = Api.load!(comment, :post_name)
+      assert comment_with_post_name.post_name == post.name
+    end
+
+    test "calculations can access attributes of parent in multitenant context" do
+      defmodule Post do
+        @moduledoc false
+        use Ash.Resource, data_layer: Ash.DataLayer.Ets
+
+        multitenancy do
+          strategy(:context)
+        end
+
+        attributes do
+          uuid_primary_key :id
+
+          attribute :name, :string
+          attribute :contents, :string
+        end
+
+        actions do
+          defaults [:read, :update, :destroy, :create]
+        end
+      end
+
+      defmodule PostName do
+        @moduledoc """
+        Calculates the name of the post, from the child comment.
+        """
+        use Ash.Calculation
+
+        @impl true
+        def load(_query, _opts, _context), do: [:post]
+
+        @impl true
+        def calculate(records, _opts, _) do
+          Enum.map(records, fn comment ->
+            post = comment.post
+            post.name
+          end)
+        end
+      end
+
+      defmodule Comment do
+        @moduledoc false
+        use Ash.Resource, data_layer: Ash.DataLayer.Ets
+
+        multitenancy do
+          strategy(:context)
+        end
+
+        attributes do
+          uuid_primary_key :id
+
+          attribute :post_id, :uuid do
+            allow_nil?(false)
+          end
+        end
+
+        actions do
+          defaults [:read, :update, :destroy, :create]
+        end
+
+        relationships do
+          belongs_to :post, Post
+        end
+
+        calculations do
+          calculate(:post_name, :string, PostName)
+        end
+      end
+
+      defmodule Registry do
+        @moduledoc false
+        use Ash.Registry
+
+        entries do
+          entry(Post)
+          entry(Comment)
+        end
+      end
+
+      defmodule MyApi do
+        @moduledoc false
+        use Ash.Api
+
+        resources do
+          registry Registry
+        end
+      end
+
+      tenant_id = "tenant1"
+
+      post =
+        Post
+        |> Ash.Changeset.for_create(:create, %{name: "Post 1", contents: "Contents 1"},
+          tenant: tenant_id
+        )
+        |> MyApi.create!()
+
+      comment =
+        Comment
+        |> Ash.Changeset.for_create(:create, %{post_id: post.id}, tenant: tenant_id)
+        |> MyApi.create!()
+
+      comment_with_post_name = Api.load!(comment, :post_name, tenant: tenant_id)
+      assert comment_with_post_name.post_name == post.name
     end
   end
 end
