d5269966697da1efd78390210ddba7a5c1fdf81b
Zach Daniel <zach@zachdaniel.dev>
2023-06-09 00:28:09 -0400

    improvement: optimize `LoadRelationship` calculation

--- lib/ash/resource/calculation/load_relationship.ex
+++ lib/ash/resource/calculation/load_relationship.ex
@@ -6,46 +6,25 @@ defmodule Ash.Resource.Calculation.LoadRelationship do
   """
   use Ash.Calculation
 
-  # TODO: This actually can be optimized to just depend on the load it needs!
-  # i.e put the logic from `calculate` into `load` and then just `Map.get` the value :)
-
   def load(query, opts, _) do
     relationship = Ash.Resource.Info.relationship(query.resource, opts[:relationship])
 
-    [relationship.source_attribute]
-  end
-
-  def calculate([], _, _), do: {:ok, []}
-
-  def calculate([%resource{} | _] = list, opts, context) do
-    api = opts[:api]
-
     query =
       opts[:query] ||
-        resource
+        query.resource
         |> Ash.Resource.Info.relationship(opts[:relationship])
         |> Map.get(:destination)
         |> Ash.Query.new()
 
     query = Ash.Query.to_query(query)
 
-    load_opts =
-      context
-      |> Map.take([:actor, :tenant, :authorize?, :tracer])
-      |> Map.to_list()
-      |> Keyword.merge(opts[:opts] || [])
+    [{relationship.name, query}]
+  end
 
-    list
-    |> api.load([{opts[:relationship], query}], load_opts)
-    |> case do
-      {:ok, values} ->
-        {:ok,
-         Enum.map(values, fn record ->
-           Map.get(record, opts[:relationship])
-         end)}
-
-      {:error, error} ->
-        {:error, error}
-    end
+  def calculate(results, opts, _context) do
+    {:ok,
+     Enum.map(results, fn record ->
+       Map.get(record, opts[:relationship])
+     end)}
   end
 end
