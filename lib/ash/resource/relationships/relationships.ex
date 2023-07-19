ce018606ac290f49d23cea5a64dadd28f06fe917
bcksl <bcksl@proton.me>
2023-07-19 12:26:13 +0300

    chore: update `picosat_elixir`

--- lib/ash/resource/relationships/relationships.ex
+++ lib/ash/resource/relationships/relationships.ex
@@ -1,8 +1,9 @@
 defmodule Ash.Resource.Relationships do
   @moduledoc "Types Ash relationships"
-  alias Ash.Resource.Relationships.{BelongsTo, HasMany, HasOne, ManyToMany}
+  alias Ash.Resource.Relationships.{BelongsTo, HasMany, HasOne, ManyToMany, PolyBelongsTo}
 
-  @type relationship :: HasOne.t() | BelongsTo.t() | HasMany.t() | ManyToMany.t()
-  @type type :: :has_many | :has_one | :belongs_to | :many_to_many
+  @type relationship ::
+          HasOne.t() | BelongsTo.t() | HasMany.t() | ManyToMany.t() | PolyBelongsTo.t()
+  @type type :: :has_many | :has_one | :belongs_to | :many_to_many | :poly_belongs_to
   @type cardinality :: :many | :one
 end
