bb31b705bf4067d073db16d9c0cad8503ddf7dd7
franckstifler <francktchowa@gmail.com>
2023-08-16 15:02:24 +0100

    Fix credo error

--- lib/ash/resource/verifiers/ensure_aggregate_field_is_attribute_or_calculation.ex
+++ lib/ash/resource/verifiers/ensure_aggregate_field_is_attribute_or_calculation.ex
@@ -8,27 +8,25 @@ defmodule Ash.Resource.Verifiers.EnsureAggregateFieldIsAttributeOrCalculation do
     aggregates = Ash.Resource.Info.aggregates(dsl)
 
     for %{field: field, relationship_path: paths} <- aggregates do
-      cond do
-        is_nil(field) ->
-          :ok
+      if is_nil(field) do
+        :ok
+      else
+        case Ash.Resource.Info.related(dsl, paths) do
+          nil ->
+            :ok
 
-        true ->
-          case Ash.Resource.Info.related(dsl, paths) do
-            nil ->
-              :ok
+          destination ->
+            attribute = Ash.Resource.Info.attribute(destination, field)
+            calculation = Ash.Resource.Info.calculation(destination, field)
 
-            destination ->
-              attribute = Ash.Resource.Info.attribute(destination, field)
-              calculation = Ash.Resource.Info.calculation(destination, field)
-
-              if !attribute and !calculation do
-                raise Spark.Error.DslError,
-                  module: Spark.Dsl.Verifier.get_persisted(dsl, :module),
-                  message:
-                    "All aggregates fields must be attributes or calculations. Got: #{inspect(field)}",
-                  path: [:aggregates, field]
-              end
-          end
+            if !attribute and !calculation do
+              raise Spark.Error.DslError,
+                module: Spark.Dsl.Verifier.get_persisted(dsl, :module),
+                message:
+                  "All aggregates fields must be attributes or calculations. Got: #{inspect(field)}",
+                path: [:aggregates, field]
+            end
+        end
       end
     end
 
