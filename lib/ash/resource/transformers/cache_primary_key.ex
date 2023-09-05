5f6920ef9256589489e2d5d04e3301c5703a56eb
James Harton <59449+jimsynz@users.noreply.github.com>
2023-09-06 10:21:42 +1200

    improvement: Allow resources to opt out of the primary key requirement. (#687)
    This is experimental and will likely cause breakages. It's to support time series tables.

--- lib/ash/resource/transformers/cache_primary_key.ex
+++ lib/ash/resource/transformers/cache_primary_key.ex
@@ -6,46 +6,71 @@ defmodule Ash.Resource.Transformers.CachePrimaryKey do
   alias Spark.Error.DslError
 
   def transform(dsl_state) do
-    primary_key_attribute =
+    attributes =
       dsl_state
       |> Transformer.get_entities([:attributes])
       |> Enum.filter(& &1.primary_key?)
 
-    pk_allows_nil? = Enum.any?(primary_key_attribute, & &1.allow_nil?)
-    simple_equality? = Enum.all?(primary_key_attribute, &Ash.Type.simple_equality?(&1.type))
+    with :ok <- validate_attributes_not_nil(attributes, dsl_state),
+         :ok <- maybe_validate_pk_present(attributes, dsl_state) do
+      dsl_state =
+        dsl_state
+        |> Transformer.persist(:primary_key, Enum.map(attributes, & &1.name))
+        |> Transformer.persist(
+          :primary_key_simple_equality?,
+          Enum.all?(attributes, &Ash.Type.simple_equality?(&1.type))
+        )
 
-    primary_key =
-      if pk_allows_nil? == false do
-        Enum.map(primary_key_attribute, & &1.name)
-      else
-        :error_pk_allows_nil
-      end
-
-    case primary_key do
-      :error_pk_allows_nil ->
-        {:error, DslError.exception(message: "Primary keys must not be allowed to be nil")}
-
-      [] ->
-        {:error,
-         DslError.exception(message: "Resources without a primary key are not yet supported")}
-
-      [field] ->
-        dsl_state =
-          dsl_state
-          |> Transformer.persist(:primary_key, [field])
-          |> Transformer.persist(:primary_key_simple_equality?, simple_equality?)
-
-        {:ok, dsl_state}
-
-      fields ->
-        dsl_state =
-          dsl_state
-          |> Transformer.persist(:primary_key, fields)
-          |> Transformer.persist(:primary_key_simple_equality?, simple_equality?)
-
-        {:ok, dsl_state}
+      {:ok, dsl_state}
     end
   end
+
+  defp validate_attributes_not_nil(attributes, dsl_state) do
+    Enum.reduce_while(attributes, :ok, fn attribute, :ok ->
+      case validate_attribute_not_nil(attribute, dsl_state) do
+        :ok -> {:cont, :ok}
+        {:error, reason} -> {:halt, {:error, reason}}
+      end
+    end)
+  end
+
+  defp validate_attribute_not_nil(attribute, dsl_state) when attribute.allow_nil? do
+    message = """
+    Primary keys must not be allowed to be nil.
+
+    The attribute `#{attribute.name}` is set as a primary key and allows nil values.
+    """
+
+    {:error,
+     DslError.exception(
+       module: Transformer.get_persisted(dsl_state, :module),
+       path: [:attributes],
+       message: message
+     )}
+  end
+
+  defp validate_attribute_not_nil(_, _), do: :ok
+
+  defp maybe_validate_pk_present([], dsl_state) do
+    if Transformer.get_option(dsl_state, [:resource], :require_primary_key?, true) do
+      message = """
+      By default, resources must have a primary key.
+
+      See `d:Ash.Resource.Dsl.resource.require_primary_key?` for more information.
+      """
+
+      {:error,
+       DslError.exception(
+         module: Transformer.get_persisted(dsl_state, :module),
+         path: [:attributes],
+         message: message
+       )}
+    else
+      :ok
+    end
+  end
+
+  defp maybe_validate_pk_present(_, _), do: :ok
 
   def after?(Ash.Resource.Transformers.BelongsToAttribute), do: true
   def after?(Ash.Resource.Transformers.DefaultPrimaryKey), do: true
