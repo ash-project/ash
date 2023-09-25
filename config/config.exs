a2c4888a26e9b580170582c076c196b49baba324
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2023-09-25 09:52:47 +0200

    cleanup config

--- config/config.exs
+++ config/config.exs
@@ -29,11 +29,4 @@ if Mix.env() == :test do
 
   config :ash, :validate_api_resource_inclusion?, false
   config :ash, :validate_api_config_inclusion?, false
-
-  config :logger, level: :debug
-
-  config :ash, :policies,
-    show_policy_breakdowns?: true,
-    log_policy_breakdowns: :info,
-    log_successful_policy_breakdowns: :info
 end
