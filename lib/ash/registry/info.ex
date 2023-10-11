2de1e81b3b14914555790dffb76be105335a2742
Zach Daniel <zach@zachdaniel.dev>
2023-10-11 09:05:56 -0400

    chore: fix more typespecs

--- lib/ash/registry/info.ex
+++ lib/ash/registry/info.ex
@@ -4,13 +4,13 @@ defmodule Ash.Registry.Info do
   alias Spark.Dsl.Extension
 
   @doc "Whether or not the registry will warn if it has no entries"
-  @spec warn_on_empty?(Ash.Registry.t()) :: boolean
+  @spec warn_on_empty?(Ash.Registry.t() | Spark.Dsl.t()) :: boolean
   def warn_on_empty?(registry) do
     Extension.get_opt(registry, [:entries], :warn_on_empty?, true, true)
   end
 
   @doc "The list of entries in the registry"
-  @spec entries(Ash.Registry.t()) :: list(module)
+  @spec entries(Ash.Registry.t() | Spark.Dsl.t()) :: list(module)
   def entries(registry) do
     case registry |> Extension.get_entities([:entries]) |> Enum.map(& &1.entry) do
       [] ->
