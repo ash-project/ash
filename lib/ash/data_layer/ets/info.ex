c89b680d4b1a68f386c070c055f56b07bc9d97aa
Zach Daniel <zach@zachdaniel.dev>
2023-10-11 09:02:07 -0400

    fix: move misplaced file and fix typespecs for Ash.DataLayer.Ets.Info

--- lib/ash/data_layer/ets/info.ex
+++ lib/ash/data_layer/ets/info.ex
@@ -4,13 +4,13 @@ defmodule Ash.DataLayer.Ets.Info do
   alias Spark.Dsl.Extension
 
   @doc "Whether or not the ets table for the resource should be private"
-  @spec private?(Ash.Resource.t()) :: boolean
+  @spec private?(Ash.Resource.t() | Spark.Dsl.t()) :: boolean
   def private?(resource) do
     Extension.get_opt(resource, [:ets], :private?, false, true)
   end
 
   @doc "The ets table name for a resource"
-  @spec table(Ash.Resource.t()) :: boolean
+  @spec table(Ash.Resource.t() | Spark.Dsl.t()) :: boolean
   def table(resource) do
     Extension.get_opt(resource, [:ets], :table, resource, true) || resource
   end
