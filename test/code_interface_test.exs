63dbf2603fce68d293717305b5896d4b1e725ff0
Zach Daniel <zach@zachdaniel.dev>
2023-10-21 23:20:13 -0400

    chore: format

--- test/code_interface_test.exs
+++ test/code_interface_test.exs
@@ -131,9 +131,12 @@ defmodule Ash.Test.CodeInterfaceTest do
         |> Ash.Changeset.for_create(:create, %{first_name: "Zach2", last_name: "Daniel2"})
         |> Api.create!()
       ]
-      assert_raise ArgumentError, ~r/^Initial must be a changeset with the action type of.+/i, fn ->
-        User.update([], %{first_name: "Zack3", last_name: "Daniel3"})
-      end
+
+      assert_raise ArgumentError,
+                   ~r/^Initial must be a changeset with the action type of.+/i,
+                   fn ->
+                     User.update([], %{first_name: "Zack3", last_name: "Daniel3"})
+                   end
     end
   end
 
