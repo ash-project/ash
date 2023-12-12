5c0275a78ba445fc1397fc454c7a0457b6a4c499
Zach Daniel <zach@zachdaniel.dev>
2023-12-11 19:57:49 -0500

    test: add test for simple data layer pagination

--- test/simple_data_layer_test.exs
+++ test/simple_data_layer_test.exs
@@ -22,7 +22,32 @@ defmodule Ash.Test.SimpleDataLayerTest do
           end)
         end
       end
+
+      read :paginated do
+        pagination offset?: true
+
+        prepare fn query, _ ->
+          Ash.Query.before_action(query, fn query ->
+            query
+            |> Ash.DataLayer.Simple.set_data(
+              [
+                struct(__MODULE__, %{id: Ash.UUID.generate(), name: "Fred"}),
+                struct(__MODULE__, %{id: Ash.UUID.generate(), name: "Fred"}),
+                struct(__MODULE__, %{id: Ash.UUID.generate(), name: "Fred"})
+              ]
+              |> offset(query.offset)
+              |> limit(query.limit)
+            )
+          end)
+        end
+      end
     end
+
+    defp limit(list, nil), do: list
+    defp limit(list, value), do: Enum.take(list, value)
+
+    defp offset(list, nil), do: list
+    defp offset(list, value), do: Enum.drop(list, value)
   end
 
   defmodule Api do
@@ -35,5 +60,10 @@ defmodule Ash.Test.SimpleDataLayerTest do
 
   test "set_data can be used in a before_action callback" do
     assert [%{name: "Fred"}] = Api.read!(Person)
+  end
+
+  test "pagination works" do
+    assert %Ash.Page.Offset{results: [%{name: "Fred"}]} =
+             Api.read!(Person, action: :paginated, page: [limit: 1])
   end
 end
