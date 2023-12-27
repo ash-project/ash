17afb821a7b59433f35fb3753f0d1d13bf7ce496
Zach Daniel <zach@zachdaniel.dev>
2023-12-27 17:37:51 -0500

    fix: don't include unknown `authorize?` value in changesets

--- test/support/policy_complex/resources/user/changes/add_friend.ex
+++ test/support/policy_complex/resources/user/changes/add_friend.ex
@@ -2,13 +2,13 @@ defmodule Ash.Test.Support.PolicyComplex.User.Changes.AddFriend do
   @moduledoc false
   use Ash.Resource.Change
 
-  def change(changeset, _, %{actor: actor, authorize?: authorize?}) do
+  def change(changeset, _, context) do
     Ash.Changeset.after_action(changeset, fn changeset, result ->
       destination_id = Ash.Changeset.get_argument(changeset, :friend_id)
 
       Ash.Test.Support.PolicyComplex.FriendLink
       |> Ash.Changeset.for_create(:create, %{source_id: result.id, destination_id: destination_id})
-      |> changeset.api.create!(actor: actor, authorize?: authorize?)
+      |> changeset.api.create!(Ash.context_to_opts(context))
 
       {:ok, result}
     end)
