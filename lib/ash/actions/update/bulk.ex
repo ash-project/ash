b3c7bc88cc0b7bdf3ee5bd87d026feaf6a34ad85
Zach Daniel <zach@zachdaniel.dev>
2024-03-09 17:00:31 -0500

    improvement!: changeset.filters -> changeset.filter

--- lib/ash/actions/update/bulk.ex
+++ lib/ash/actions/update/bulk.ex
@@ -1583,7 +1583,7 @@ defmodule Ash.Actions.Update.Bulk do
 
       batch ->
         batch
-        |> Enum.group_by(&{&1.atomics, &1.filters})
+        |> Enum.group_by(&{&1.atomics, &1.filter})
         |> Enum.flat_map(fn {_atomics, batch} ->
           result =
             case action.manual do
