54bbcc3057d36406418447cb8e13377650599dc9
Zach Daniel <zach@zachdaniel.dev>
2024-03-12 20:32:12 -0400

    chore: I was wrong

--- lib/ash/actions/read/read.ex
+++ lib/ash/actions/read/read.ex
@@ -1543,7 +1543,7 @@ defmodule Ash.Actions.Read do
             (opts[:page][:count] != false and action.pagination.countable == :by_default)) do
       with {:ok, filter} <-
              filter_with_related(
-               query_before_pagination,
+               query,
                opts[:authorize?],
                relationship_path_filters
              ),
