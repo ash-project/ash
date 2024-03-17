818b5227d4681554405b1783df943f2a9b711728
WIGGLES <55168935+WIGGLES-dev@users.noreply.github.com>
2024-03-17 12:44:12 -0400

    fix: set tenant on query so that root calles to Api.aggreagte work as expected (#929)

--- lib/ash/actions/aggregate.ex
+++ lib/ash/actions/aggregate.ex
@@ -76,7 +76,8 @@ defmodule Ash.Actions.Aggregate do
                      Ash.Query.data_layer_query(%Ash.Query{
                        resource: query.resource,
                        limit: query.limit,
-                       offset: query.offset
+                       offset: query.offset,
+                       tenant: query.tenant
                      }),
                    {:ok, result} <-
                      Ash.DataLayer.run_aggregate_query(
