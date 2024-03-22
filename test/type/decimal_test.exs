75b2d2eb009964e248c0993509903b59012eda48
Minsub Kim <rapidfsub@gmail.com>
2024-03-22 15:39:59 +0900

    feat: add less_than, greater_than constraints to float, decimal (#936)

---
+++ test/type/decimal_test.exs
@@ -0,0 +1,70 @@
+defmodule Ash.Test.Type.DecimalTest do
+  @moduledoc false
+  use ExUnit.Case, async: true
+
+  import Ash.Changeset
+  require Ash.Query
+
+  defmodule Balance do
+    @moduledoc false
+    use Ash.Resource, data_layer: Ash.DataLayer.Ets
+
+    ets do
+      private?(true)
+    end
+
+    actions do
+      defaults [:create, :read, :update, :destroy]
+    end
+
+    attributes do
+      uuid_primary_key :id
+      attribute :amount, :decimal, allow_nil?: false
+      attribute :positive_amount, :decimal, allow_nil?: false, constraints: [greater_than: 0]
+      attribute :negative_amount, :decimal, allow_nil?: false, constraints: [less_than: 0]
+    end
+  end
+
+  defmodule Api do
+    @moduledoc false
+    use Ash.Api
+
+    resources do
+      resource Balance
+    end
+  end
+
+  @valid_attrs %{
+    amount: 1.0,
+    positive_amount: 1.0,
+    negative_amount: -1.0
+  }
+
+  test "pass with valid attrs" do
+    assert Balance
+           |> new(@valid_attrs)
+           |> Api.create!()
+  end
+
+  test "fail with invalid positive_amount" do
+    for positive_amount <- [0, -1] do
+      invalid_attrs = @valid_attrs |> Map.put(:positive_amount, positive_amount)
+
+      assert {:error, %Ash.Error.Invalid{}} =
+               Balance
+               |> new(invalid_attrs)
+               |> Api.create()
+    end
+  end
+
+  test "fail with invalid negative_amount" do
+    for negative_amount <- [0, 1] do
+      invalid_attrs = @valid_attrs |> Map.put(:negative_amount, negative_amount)
+
+      assert {:error, %Ash.Error.Invalid{}} =
+               Balance
+               |> new(invalid_attrs)
+               |> Api.create()
+    end
+  end
+end
