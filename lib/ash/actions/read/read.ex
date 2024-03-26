e29fe9d93205e51c959fbff7a1a8b16f5f672855
Zach Daniel <zach@zachdaniel.dev>
2024-03-26 02:05:53 -0400

    chore: don't reselect when missing primary keys

--- lib/ash/actions/read/read.ex
+++ lib/ash/actions/read/read.ex
@@ -688,7 +688,7 @@ defmodule Ash.Actions.Read do
            ])
            |> Ash.Query.load(calculations_in_query)
            |> Ash.Query.select(must_be_reselected)
-           |> Ash.DataLayer.Simple.set_data(initial_data)
+           |> Ash.DataLayer.Simple.set_data(IO.inspect(initial_data))
            |> Ash.Query.do_filter(filter),
          {:ok, data_layer_calculations} <-
            hydrate_calculations(
@@ -859,18 +859,16 @@ defmodule Ash.Actions.Read do
          authorize?,
          attrs? \\ true
        ) do
-    load_through =
-      query.resource
-      |> Ash.Resource.Info.attributes()
-      |> Enum.filter(fn %{name: name, type: type, constraints: constraints} ->
-        Ash.Type.can_load?(type, constraints) && Ash.Query.selecting?(query, name)
-      end)
-      |> Enum.map(& &1.name)
-      |> Enum.reduce(query.load_through, fn name, load_through ->
-        Map.update(load_through, :attribute, %{name => []}, &Map.put_new(&1, name, []))
-      end)
-
-    Enum.reduce_while(load_through, {:ok, results}, fn
+    query.resource
+    |> Ash.Resource.Info.attributes()
+    |> Enum.filter(fn %{name: name, type: type, constraints: constraints} ->
+      Ash.Type.can_load?(type, constraints) && Ash.Query.selecting?(query, name)
+    end)
+    |> Enum.map(& &1.name)
+    |> Enum.reduce(query.load_through, fn name, load_through ->
+      Map.update(load_through, :attribute, %{name => []}, &Map.put_new(&1, name, []))
+    end)
+    |> Enum.reduce_while({:ok, results}, fn
       {:calculation, load_through}, {:ok, results} ->
         load_through
         |> Map.take(Map.keys(query.calculations))
