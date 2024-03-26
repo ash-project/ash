91a14cb221c61a826c7a8039059a338bf7bbf897
Zach Daniel <zach@zachdaniel.dev>
2024-03-26 13:34:39 -0400

    chore: update spark

--- mix.exs
+++ mix.exs
@@ -303,7 +303,7 @@ defmodule Ash.MixProject do
   defp deps do
     [
       # DSLs
-      {:spark, "~> 2.1 and >= 2.1.4"},
+      {:spark, "~> 2.1 and >= 2.1.6"},
       # Ash resources are backed by ecto scheams
       {:ecto, "~> 3.7"},
       # Used by the ETS data layer
