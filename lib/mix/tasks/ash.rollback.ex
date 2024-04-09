4f26773fe04697ee0234413ff6403b52111a7b35
Zach Daniel <zach@zachdaniel.dev>
2024-04-09 14:54:08 -0400

    improvement: add `ash.rollback` task

---
+++ lib/mix/tasks/ash.rollback.ex
@@ -0,0 +1,27 @@
+defmodule Mix.Tasks.Ash.Rollback do
+  @moduledoc """
+  Runs all rollback tasks for any extension on any resource/domain in your application.
+  """
+  use Mix.Task
+
+  @shortdoc "Runs all rollback tasks for any extension on any resource/domain in your application."
+  @doc @shortdoc
+  def run(argv) do
+    argv
+    |> Ash.Mix.Tasks.Helpers.extensions!()
+    |> Enum.map(fn extension ->
+      if function_exported?(extension, :rollback, 1) do
+        name =
+          if function_exported?(extension, :name, 0) do
+            extension.name()
+          else
+            inspect(extension)
+          end
+
+        Mix.shell().info("Running rollback for #{name}...")
+
+        extension.rollback(argv)
+      end
+    end)
+  end
+end
