a0465cd7f6bef9514e137c52d90b22012a0a5677
James Harton <james@harton.nz>
2024-04-10 14:25:43 +1200

    chore: release version v2.21.10

--- README.md
+++ README.md
@@ -10,7 +10,7 @@
 ```elixir
 def deps do
   [
-    {:ash, "~> 2.21.9"}
+    {:ash, "~> 2.21.10"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Ash.MixProject do
   designed to be used by multiple front ends.
   """
 
-  @version "2.21.9"
+  @version "2.21.10"
 
   def project do
     [
