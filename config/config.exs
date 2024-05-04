c0cf401b3bfa4ac24f0c27ee8fd09b7c87429758
Zach Daniel <zach@zachdaniel.dev>
2024-05-04 01:12:48 -0400

    chore: small docs tweaks

--- config/config.exs
+++ config/config.exs
@@ -10,7 +10,7 @@ if Mix.env() == :dev do
     manage_mix_version?: true,
     # Instructs the tool to manage the version in your README.md
     # Pass in `true` to use `"README.md"` or a string to customize
-    manage_readme_version: ["README.md", "documentation/tutorials/get-started.md"],
+    manage_readme_version: ["README.md", "documentation/tutorials/get-started.md", "documentation/how-to/cook-book/optimistic-locking.livemd"],
     version_tag_prefix: "v"
 end
 
