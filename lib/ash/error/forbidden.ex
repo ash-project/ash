1d519e37e07b88fa1089637a83ba7ba43559de44
Zach Daniel <zach@zachdaniel.dev>
2024-05-06 20:58:39 -0400

    chore: better inspecting for error classes

--- lib/ash/error/forbidden.ex
+++ lib/ash/error/forbidden.ex
@@ -3,4 +3,67 @@ defmodule Ash.Error.Forbidden do
   use Splode.ErrorClass, fields: [:changeset, :query, :action_input], class: :forbidden
 
   @type t :: %__MODULE__{}
+  defimpl Inspect do
+    import Inspect.Algebra
+
+    def inspect(
+          %{
+            changeset: changeset,
+            query: query,
+            action_input: action_input,
+            errors: errors,
+            bread_crumbs: bread_crumbs,
+            vars: vars,
+            path: path
+          },
+          opts
+        ) do
+      changeset =
+        if changeset do
+          "#Changeset<>"
+        else
+          nil
+        end
+
+      query =
+        if query do
+          "#Query<>"
+        else
+          nil
+        end
+
+      action_input =
+        if action_input do
+          "#ActionInput<>"
+        else
+          nil
+        end
+
+      items =
+        [
+          bread_crumbs: bread_crumbs,
+          path: path,
+          vars: vars,
+          changeset: changeset,
+          query: query,
+          action_input: action_input,
+          errors: errors
+        ]
+        |> Enum.reject(fn {_key, item} ->
+          item in [nil, []]
+        end)
+        |> Enum.map(fn {key, value} ->
+          {key, to_doc(value, opts)}
+        end)
+
+      container_doc(
+        "%Ash.Error.Forbidden{",
+        items,
+        "}",
+        opts,
+        fn {key, val}, _ -> concat(["#{key}: ", val]) end,
+        separator: ", "
+      )
+    end
+  end
 end
--- lib/ash/error/framework.ex
+++ lib/ash/error/framework.ex
@@ -3,4 +3,68 @@ defmodule Ash.Error.Framework do
   use Splode.ErrorClass, fields: [:changeset, :query, :action_input], class: :framework
 
   @type t :: %__MODULE__{}
+
+  defimpl Inspect do
+    import Inspect.Algebra
+
+    def inspect(
+          %{
+            changeset: changeset,
+            query: query,
+            action_input: action_input,
+            errors: errors,
+            bread_crumbs: bread_crumbs,
+            vars: vars,
+            path: path
+          },
+          opts
+        ) do
+      changeset =
+        if changeset do
+          "#Changeset<>"
+        else
+          nil
+        end
+
+      query =
+        if query do
+          "#Query<>"
+        else
+          nil
+        end
+
+      action_input =
+        if action_input do
+          "#ActionInput<>"
+        else
+          nil
+        end
+
+      items =
+        [
+          bread_crumbs: bread_crumbs,
+          path: path,
+          vars: vars,
+          changeset: changeset,
+          query: query,
+          action_input: action_input,
+          errors: errors
+        ]
+        |> Enum.reject(fn {_key, item} ->
+          item in [nil, []]
+        end)
+        |> Enum.map(fn {key, value} ->
+          {key, to_doc(value, opts)}
+        end)
+
+      container_doc(
+        "%Ash.Error.framework{",
+        items,
+        "}",
+        opts,
+        fn {key, val}, _ -> concat(["#{key}: ", val]) end,
+        separator: ", "
+      )
+    end
+  end
 end
