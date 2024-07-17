dad0bfd1c4982cdae3e8c84c4bda943dc42e2ad4
Hannes Wüthrich <hannes.wuethrich@zebbra.ch>
2024-07-17 15:52:12 +0200

    fix: handle casting nil vectors (#1316)

--- test/type/vector_test.exs
+++ test/type/vector_test.exs
@@ -9,4 +9,10 @@ defmodule Ash.Test.Type.VectorTest do
     assert {:ok, ^vector} = Ash.Type.dump_to_native(Ash.Type.Vector, list)
     assert {:ok, ^vector} = Ash.Type.dump_to_native(Ash.Type.Vector, vector)
   end
+
+  test "it casts nil to nil" do
+    assert {:ok, nil} = Ash.Type.cast_input(Ash.Type.Vector, nil)
+    assert {:ok, nil} = Ash.Type.cast_stored(Ash.Type.Vector, nil)
+    assert {:ok, nil} = Ash.Type.dump_to_native(Ash.Type.Vector, nil)
+  end
 end
