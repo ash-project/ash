0bdb3acfe2021ddf517fa49581b32e16397fdd9d
Zach Daniel <zach@zachdaniel.dev>
2024-07-25 09:32:16 -0400

    fix: properly handle non-domain modules or modules w/ no resources
    closes #1345

--- lib/ash/domain/igniter.ex
+++ lib/ash/domain/igniter.ex
@@ -18,44 +18,53 @@ defmodule Ash.Domain.Igniter do
 
   @doc "Adds a resource reference to a domain's `resources` block"
   def add_resource_reference(igniter, domain, resource) do
-    Igniter.Code.Module.find_and_update_module!(igniter, domain, fn zipper ->
-      case Igniter.Code.Function.move_to_function_call_in_current_scope(
-             zipper,
-             :resources,
-             1
-           ) do
-        :error ->
-          code =
-            """
-            resources do
-              resource #{inspect(resource)}
+    {igniter, domains} = Ash.Domain.Igniter.list_domains(igniter)
+
+    if domain in domains do
+      Igniter.Code.Module.find_and_update_module!(igniter, domain, fn zipper ->
+        case Igniter.Code.Function.move_to_function_call_in_current_scope(
+               zipper,
+               :resources,
+               1
+             ) do
+          :error ->
+            code =
+              """
+              resources do
+                resource #{inspect(resource)}
+              end
+              """
+
+            {:ok, Igniter.Code.Common.add_code(zipper, code)}
+
+          {:ok, zipper} ->
+            with {:ok, zipper} <- Igniter.Code.Common.move_to_do_block(zipper),
+                 :error <-
+                   Igniter.Code.Function.move_to_function_call_in_current_scope(
+                     zipper,
+                     :resource,
+                     1,
+                     fn call ->
+                       Igniter.Code.Function.argument_matches_predicate?(
+                         call,
+                         0,
+                         &Igniter.Code.Common.nodes_equal?(&1, resource)
+                       )
+                     end
+                   ) do
+              {:ok, Igniter.Code.Common.add_code(zipper, "resource #{inspect(resource)}")}
+            else
+              _ ->
+                {:ok, zipper}
             end
-            """
-
-          Igniter.Code.Common.add_code(zipper, code)
-
-        {:ok, zipper} ->
-          with {:ok, zipper} <- Igniter.Code.Common.move_to_do_block(zipper),
-               :error <-
-                 Igniter.Code.Function.move_to_function_call_in_current_scope(
-                   zipper,
-                   :resource,
-                   1,
-                   fn call ->
-                     Igniter.Code.Function.argument_matches_predicate?(
-                       call,
-                       0,
-                       &Igniter.Code.Common.nodes_equal?(&1, resource)
-                     )
-                   end
-                 ) do
-            {:ok, Igniter.Code.Common.add_code(zipper, "resource #{inspect(resource)}")}
-          else
-            _ ->
-              {:ok, zipper}
-          end
-      end
-    end)
+        end
+      end)
+    else
+      igniter
+      |> Igniter.add_warning(
+        "Domain #{domain} was not an `Ash.Domain`, so could not add `#{inspect(resource)}` to its resource list."
+      )
+    end
   end
 
   @doc "Removes a resource reference from a domain's `resources` block"
