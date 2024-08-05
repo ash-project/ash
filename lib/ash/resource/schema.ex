bed5a57e58c0c4bffd89a833a8128577b3bb862c
Zach Daniel <zachary.s.daniel@gmail.com>
2024-08-05 10:34:46 -0400

    Update lib/ash/resource/schema.ex

--- lib/ash/resource/schema.ex
+++ lib/ash/resource/schema.ex
@@ -274,7 +274,9 @@ defmodule Ash.Schema do
             Enum.each(struct_fields, &Module.put_attribute(__MODULE__, struct_fields_name, &1))
           end
 
-          @type t :: %__MODULE__{}
+      unless Kernel.Typespec.defines_type?(__MODULE__, {:t, 0}) do
+        @type t :: %__MODULE__{}
+      end
 
           after_compile =
             @after_compile -- [{Ecto.Schema, :__after_compile__}]
