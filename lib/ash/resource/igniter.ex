bf741990a2e78ef1ebdbd28fce66364fb8adc7dd
Zach Daniel <zach@zachdaniel.dev>
2024-09-15 14:53:23 -0400

    improvement: add `_new` options for Ash.Resource.Igniter

--- lib/ash/resource/igniter.ex
+++ lib/ash/resource/igniter.ex
@@ -1,5 +1,14 @@
 defmodule Ash.Resource.Igniter do
-  @moduledoc "Codemods for working with Ash.Resource modules"
+  @moduledoc """
+  Codemods for working with Ash.Resource modules
+
+  ## Important Details
+
+  This interrogates *the source code* of a resource, not its ultimate compiled state.
+  What this means, is that things like `defines_attribute?` will not return `true` if
+  the attribute is added by an extension. Only if it appears literally in the source code
+  of the resource or one of its `Spark.Dsl.Fragment`s.
+  """
 
   @doc "List all resource modules found in the project"
   def list_resources(igniter) do
@@ -147,14 +156,139 @@ defmodule Ash.Resource.Igniter do
     end)
   end
 
+  @doc "Returns true if the given resource defines an attribute with the provided name"
+  @spec defines_attribute(Igniter.t(), Ash.Resource.t(), atom()) :: {Igniter.t(), true | false}
+  def defines_attribute(igniter, resource, name) do
+    Spark.Igniter.find(igniter, resource, fn _, zipper ->
+      with {:ok, zipper} <- enter_section(zipper, :attributes),
+           {:ok, _zipper} <-
+             Igniter.Code.Function.move_to_function_call_in_current_scope(
+               zipper,
+               :attribute,
+               [2, 3],
+               &Igniter.Code.Function.argument_equals?(&1, 0, name)
+             ) do
+        {:ok, true}
+      else
+        _ ->
+          :error
+      end
+    end)
+    |> case do
+      {:ok, igniter, _module, _value} ->
+        {igniter, true}
+
+      {:error, igniter} ->
+        {igniter, false}
+    end
+  end
+
+  @doc "Returns true if the given resource defines an action with the provided name"
+  @spec defines_action(Igniter.t(), Ash.Resource.t(), atom()) :: {Igniter.t(), true | false}
+  def defines_action(igniter, resource, name) do
+    Spark.Igniter.find(igniter, resource, fn _, zipper ->
+      with {:ok, zipper} <- enter_section(zipper, :actions),
+           {:ok, _zipper} <-
+             move_to_one_of_function_call_in_current_scope(
+               zipper,
+               [
+                 :create,
+                 :update,
+                 :read,
+                 :destroy,
+                 :action
+               ],
+               [2, 3, 4],
+               &Igniter.Code.Function.argument_equals?(&1, 0, name)
+             ) do
+        {:ok, true}
+      else
+        _ ->
+          :error
+      end
+    end)
+    |> case do
+      {:ok, igniter, _module, _value} ->
+        {igniter, true}
+
+      {:error, igniter} ->
+        {igniter, false}
+    end
+  end
+
+  @doc "Returns true if the given resource defines a relationship with the provided name"
+  @spec defines_relationship(Igniter.t(), Ash.Resource.t(), atom()) :: {Igniter.t(), true | false}
+  def defines_relationship(igniter, resource, name) do
+    Spark.Igniter.find(igniter, resource, fn _, zipper ->
+      with {:ok, zipper} <- enter_section(zipper, :relationships),
+           {:ok, _zipper} <-
+             move_to_one_of_function_call_in_current_scope(
+               zipper,
+               [
+                 :has_one,
+                 :has_many,
+                 :belongs_to,
+                 :many_to_many
+               ],
+               [2, 3],
+               &Igniter.Code.Function.argument_equals?(&1, 0, name)
+             ) do
+        {:ok, true}
+      else
+        _ ->
+          :error
+      end
+    end)
+    |> case do
+      {:ok, igniter, _module, _value} ->
+        {igniter, true}
+
+      {:error, igniter} ->
+        {igniter, false}
+    end
+  end
+
+  @doc "Adds the given code block to the resource's `attributes` block"
+  def add_new_attribute(igniter, resource, name, attribute) do
+    {igniter, defines?} = defines_attribute(igniter, resource, name)
+
+    if defines? do
+      igniter
+    else
+      add_attribute(igniter, resource, attribute)
+    end
+  end
+
   @doc "Adds the given code block to the resource's `attributes` block"
   def add_attribute(igniter, resource, attribute) do
     add_block(igniter, resource, :attributes, attribute)
   end
 
+  @doc "Adds an action if it doesn't already exist"
+  def add_new_action(igniter, resource, name, action) do
+    {igniter, defines?} = defines_action(igniter, resource, name)
+
+    if defines? do
+      igniter
+    else
+      add_action(igniter, resource, action)
+    end
+  end
+
   @doc "Adds the given code block to the resource's `actions` block"
   def add_action(igniter, resource, action) do
     add_block(igniter, resource, :actions, action)
+  end
+
+  @doc "Adds the given code block to the resource's `relationships` block"
+  def add_new_relationship(igniter, resource, name, relationship) do
+    {igniter, defines?} = defines_relationship(igniter, resource, name)
+
+    if defines? do
+      igniter
+    else
+      add_block(igniter, resource, :relationships, relationship)
+    end
   end
 
   @doc "Adds the given code block to the resource's `relationships` block"
@@ -165,5 +299,37 @@ defmodule Ash.Resource.Igniter do
   @doc "Adds the given code block to the resource's `resource` block"
   def add_resource_configuration(igniter, resource, resource_configuration) do
     add_block(igniter, resource, :resource, resource_configuration)
+  end
+
+  defp enter_section(zipper, name) do
+    with {:ok, zipper} <-
+           Igniter.Code.Function.move_to_function_call_in_current_scope(
+             zipper,
+             name,
+             1
+           ) do
+      Igniter.Code.Common.move_to_do_block(zipper)
+    end
+  end
+
+  defp move_to_one_of_function_call_in_current_scope(zipper, [name], arities, pred) do
+    Igniter.Code.Function.move_to_function_call_in_current_scope(
+      zipper,
+      name,
+      arities,
+      pred
+    )
+  end
+
+  defp move_to_one_of_function_call_in_current_scope(zipper, [name | rest], arities, pred) do
+    case Igniter.Code.Function.move_to_function_call_in_current_scope(
+           zipper,
+           name,
+           arities,
+           pred
+         ) do
+      {:ok, zipper} -> {:ok, zipper}
+      :error -> move_to_one_of_function_call_in_current_scope(zipper, rest, arities, pred)
+    end
   end
 end
---
+++ test/resource/igniter_test.exs
@@ -0,0 +1,112 @@
+defmodule Ash.Resource.IgniterTest do
+  use ExUnit.Case
+
+  import Igniter.Test
+
+  describe "add_new_attribute" do
+    test "adds an attribute if it doesnt exists" do
+      test_project()
+      |> Igniter.Project.Module.create_module(MyApp.User, """
+      use Ash.Resource
+      """)
+      |> apply_igniter!()
+      |> Ash.Resource.Igniter.add_new_attribute(MyApp.User, :name, """
+      attribute :name, :string, allow_nil?: true
+      """)
+      |> assert_has_patch("lib/my_app/user.ex", """
+      + | attribute :name, :string, allow_nil?: true
+      """)
+    end
+
+    test "doesnt an attribute if it exists" do
+      test_project()
+      |> Igniter.Project.Module.create_module(MyApp.User, """
+      use Ash.Resource
+
+      attributes do
+        attribute :name, :string, allow_nil?: false
+      end
+      """)
+      |> apply_igniter!()
+      |> Ash.Resource.Igniter.add_new_attribute(MyApp.User, :name, """
+      attribute :name, :string, allow_nil?: true
+      """)
+      |> assert_unchanged()
+    end
+
+    test "doesnt add an attribute if it exists in a fragment" do
+      test_project()
+      |> Igniter.Project.Module.create_module(MyApp.User, """
+      use Ash.Resource, fragments: [MyApp.User.Attributes]
+      """)
+      |> Igniter.Project.Module.create_module(MyApp.User.Attributes, """
+      use Spark.Dsl.Fragment, of: Ash.Resource
+
+      attributes do
+        attribute :name, :string, allow_nil?: false
+      end
+      """)
+      |> apply_igniter!()
+      |> Ash.Resource.Igniter.add_new_attribute(MyApp.User, :name, """
+      attribute :name, :string, allow_nil?: true
+      """)
+      |> assert_unchanged("lib/my_app/user.ex")
+    end
+  end
+
+  describe "add_new_action" do
+    test "add_new_action adds an attribute if it doesnt exists" do
+      test_project()
+      |> Igniter.Project.Module.create_module(MyApp.User, """
+      use Ash.Resource
+      """)
+      |> apply_igniter!()
+      |> Ash.Resource.Igniter.add_new_action(MyApp.User, :create, """
+        create :create do
+        end
+      """)
+      |> assert_has_patch("lib/my_app/user.ex", """
+      + | create :create
+      """)
+    end
+
+    test "add_new_action doesnt an attribute if it exists" do
+      test_project()
+      |> Igniter.Project.Module.create_module(MyApp.User, """
+      use Ash.Resource
+
+      actions do
+        create :create do
+        end
+      end
+      """)
+      |> apply_igniter!()
+      |> Ash.Resource.Igniter.add_new_action(MyApp.User, :create, """
+      create :create do
+      end
+      """)
+      |> assert_unchanged()
+    end
+
+    test "add_new_action doesnt add an action if it exists in a fragment" do
+      test_project()
+      |> Igniter.Project.Module.create_module(MyApp.User, """
+      use Ash.Resource, fragments: [MyApp.User.Actions]
+      """)
+      |> Igniter.Project.Module.create_module(MyApp.User.Actions, """
+      use Spark.Dsl.Fragment, of: Ash.Resource
+
+      actions do
+        create :create do
+        end
+      end
+      """)
+      |> apply_igniter!()
+      |> Ash.Resource.Igniter.add_new_action(MyApp.User, :create, """
+      create :create do
+      end
+      """)
+      |> assert_unchanged()
+    end
+  end
+end
