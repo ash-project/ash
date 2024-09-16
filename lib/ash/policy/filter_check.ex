4b749c4d1bb7768f1422383a0f8d0e3e9d91603e
Zach Daniel <zach@zachdaniel.dev>
2024-09-16 10:04:32 -0400

    fix: ensure that `resource` context is set for expanding filter descriptions

--- lib/ash/policy/filter_check.ex
+++ lib/ash/policy/filter_check.ex
@@ -344,13 +344,13 @@ defmodule Ash.Policy.FilterCheck do
       end
 
       def expand_description(actor, authorizer, opts) do
-        opts = Keyword.put_new(opts, :resource, authorizer.resource)
-
         changeset =
           case authorizer.subject do
             %Ash.Changeset{} = changeset -> changeset
             _ -> nil
           end
+
+        opts = Keyword.put_new(opts, :resource, authorizer.resource)
 
         {:ok,
          actor
