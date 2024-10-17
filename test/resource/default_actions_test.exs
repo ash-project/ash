9f3c40544ff8e008e895ea7300127a5013bf9132
James Harton <james@harton.nz>
2024-10-17 19:27:00 +1300

    improvement: Conditionally enable transactions on default actions.
    When the resource's data layer doesn't support transactions the generated default actions don't enable transactions.

---
+++ test/resource/default_actions_test.exs
@@ -0,0 +1,58 @@
+defmodule Ash.Test.Resource.DefaultActionsTest do
+  @moduledoc false
+  use ExUnit.Case, async: true
+  use Mimic
+
+  test "when the data layer does not support transactions, it doesn't enable them on the generated default actions" do
+    defmodule TransactionFree do
+      @moduledoc false
+      use Ash.Resource, domain: Ash.Test.Domain, data_layer: Ash.DataLayer.Ets
+
+      attributes do
+        uuid_primary_key :id
+      end
+
+      actions do
+        defaults [:read, :destroy, create: :*, update: :*]
+      end
+    end
+
+    transacting_actions =
+      TransactionFree
+      |> Ash.Resource.Info.actions()
+      |> Enum.filter(&(&1.transaction? == true))
+      |> Enum.map(& &1.name)
+
+    assert transacting_actions == []
+  end
+
+  test "when the data layer does support transactions, it enables them on the generated default actions" do
+    Ash.DataLayer
+    |> Mimic.stub(:can?, fn
+      :transact, _ -> true
+      cap, resource -> call_original(Ash.DataLayer, :can?, [cap, resource])
+    end)
+
+    defmodule TransactionFiend do
+      @moduledoc false
+      use Ash.Resource, domain: Ash.Test.Domain, data_layer: Ash.DataLayer.Ets
+
+      attributes do
+        uuid_primary_key :id
+      end
+
+      actions do
+        defaults [:read, :destroy, create: :*, update: :*]
+      end
+    end
+
+    transacting_actions =
+      TransactionFiend
+      |> Ash.Resource.Info.actions()
+      |> Enum.filter(&(&1.transaction? == true))
+      |> Enum.map(& &1.name)
+      |> Enum.sort()
+
+    assert transacting_actions == [:create, :destroy, :update]
+  end
+end
