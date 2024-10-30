a4a939045b9729e838cdaa79d3986c40bb691a88
Zach Daniel <zach@zachdaniel.dev>
2024-10-29 21:41:57 -0400

    chore: fix warning

--- lib/ash/type/union.ex
+++ lib/ash/type/union.ex
@@ -696,7 +696,7 @@ defmodule Ash.Type.Union do
   end
 
   @impl true
-  def handle_change(nil, nil, constraints),
+  def handle_change(nil, nil, _constraints),
     do: {:ok, nil}
 
   def handle_change(nil, %Ash.Union{type: type_name, value: new_value}, constraints),
