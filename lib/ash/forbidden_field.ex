8fb23e848be7fd8da2e1dc3e3aed3e7c4692ad28
Allen Wyma <>
2024-11-24 21:45:27 +0800

    add in typespecs for foribidden field

--- lib/ash/forbidden_field.ex
+++ lib/ash/forbidden_field.ex
@@ -1,5 +1,11 @@
 defmodule Ash.ForbiddenField do
   @moduledoc "Represents a field that was hidden due to authorization rules."
+  @type field_type :: :aggregate | :attribute | :calculation
+  @type t :: %__MODULE__{
+          field: atom(),
+          original_value: term(),
+          field: field_type()
+        }
   @derive {Inspect, only: [:field, :type]}
   # original_value is unfortunately required for embedded attributes, although hidden
   # embedded resources have to be able to be written back, even if the user can't see
