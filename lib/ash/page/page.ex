25956dd63839746566f2347de5bbf9076d862c78
Allen Wyma <>
2024-11-25 18:31:54 +0800

    Add some docs and typespecs for Ash.Page

--- lib/ash/page/page.ex
+++ lib/ash/page/page.ex
@@ -6,18 +6,17 @@ defmodule Ash.Page do
 
   @doc false
   # This is a custom validator for an options schema
+  @spec page_opts(page() | false | nil | Keyword.t()) :: {:ok, page()} | {:error, String.t()}
+  def page_opts(term) when term in [false, nil], do: {:ok, term}
+
   def page_opts(page_opts) do
-    if page_opts in [false, nil] do
-      {:ok, page_opts}
+    if page_opts[:after] || page_opts[:before] do
+      validate_or_error(page_opts, Ash.Page.Keyset.Opts)
     else
-      if page_opts[:after] || page_opts[:before] do
-        validate_or_error(page_opts, Ash.Page.Keyset.Opts)
+      if page_opts[:offset] do
+        validate_or_error(page_opts, Ash.Page.Offset.Opts)
       else
-        if page_opts[:offset] do
-          validate_or_error(page_opts, Ash.Page.Offset.Opts)
-        else
-          validate_or_error(page_opts, Ash.Page.Keyset.Opts)
-        end
+        validate_or_error(page_opts, Ash.Page.Keyset.Opts)
       end
     end
   end
--- lib/ash/page/unpaged.ex
+++ lib/ash/page/unpaged.ex
@@ -2,9 +2,17 @@ defmodule Ash.Page.Unpaged do
   # Utility struct to store all the info for a paged relationship to be first split between
   # related records and then paged
   @moduledoc false
+  @type t :: %__MODULE__{
+          related_records: Ash.Resource.record(),
+          opts: Keyword.t()
+        }
 
   defstruct [:related_records, :opts]
 
+  @doc """
+  Creates a new `Ash.Page.Unpaged.t`.
+  """
+  @spec new([Ash.Resource.record()], Keyword.t()) :: t()
   def new(related_records, opts) do
     %__MODULE__{
       related_records: related_records,
