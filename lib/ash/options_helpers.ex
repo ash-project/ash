0c6967df219414256839cfc1744845c80d72a223
Allen Wyma <>
2024-11-25 13:04:03 +0800

    document options helper functions

--- lib/ash/options_helpers.ex
+++ lib/ash/options_helpers.ex
@@ -3,20 +3,29 @@ defmodule Ash.OptionsHelpers do
   Helpers for working with nimble options
   """
 
-  @doc false
+  @doc """
+  Specifies Ash types that can be allowed when declaring a type for an
+  attribute, calculation, aggregate, etc.
+  """
   @spec ash_type() :: {:spark_type, Ash.Type, :builtins, [{String.t(), String.t(), String.t()}]}
   def ash_type do
     {:spark_type, Ash.Type, :builtins,
      [{"{:array, inner_type}", "list", "{:array, ${1:inner_type}}"}]}
   end
 
-  @doc false
+  @doc """
+  Used to configure Spark to know that the type referenced is an Ash.Resource
+  also using Spark.
+  """
   @spec ash_resource() :: {:spark, Ash.Resource}
   def ash_resource do
     {:spark, Ash.Resource}
   end
 
-  @doc false
+  @doc """
+  Used for marking an option as hidden so it doesn't show up as an option
+  with Spark.
+  """
   @spec hide_all_except(Keyword.t(), [atom()]) :: Keyword.t()
   def hide_all_except(options, keys) do
     Enum.map(options, fn {key, config} ->
