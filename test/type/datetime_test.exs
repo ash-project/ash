df765c7869ffd1050492810533bf8267af965672
Zach Daniel <zach@zachdaniel.dev>
2025-01-10 15:03:40 -0500

    improvement: add start_of_day function improvement: add `cast_dates_as` constraint to `Ash.Type.DateTime`
    - Added `Ash.Query.Function.StartOfDay` module to convert date or datetime to the start of the day.
    - Updated `Ash.Type.DateTime` to include `cast_dates_as` constraint with options `:start_of_day` and `:error`.

---
+++ test/type/datetime_test.exs
@@ -0,0 +1,58 @@
+defmodule Ash.Test.Type.DateTimeTest do
+  @moduledoc false
+  use ExUnit.Case, async: true
+
+  alias Ash.Test.Domain, as: Domain
+
+  defmodule Post do
+    @moduledoc false
+    use Ash.Resource, domain: Domain, data_layer: Ash.DataLayer.Ets
+
+    ets do
+      private?(true)
+    end
+
+    actions do
+      default_accept :*
+      defaults [:read, :destroy, create: :*, update: :*]
+    end
+
+    attributes do
+      uuid_primary_key :id
+
+      attribute :utc_datetime, :utc_datetime do
+        public?(true)
+      end
+
+      attribute :utc_datetime_no_dates, :utc_datetime do
+        constraints cast_dates_as: :error
+        public?(true)
+      end
+    end
+  end
+
+  test "it allows dates by default" do
+    today = Date.utc_today()
+
+    post =
+      Post
+      |> Ash.Changeset.for_create(:create, %{
+        utc_datetime: today
+      })
+      |> Ash.create!()
+
+    assert DateTime.to_date(post.utc_datetime) == today
+  end
+
+  test "it does not allow dates if `cast_dates_as` is `:error`" do
+    today = Date.utc_today()
+
+    assert_raise Ash.Error.Invalid, ~r"must be a datetime, got a date", fn ->
+      Post
+      |> Ash.Changeset.for_create(:create, %{
+        utc_datetime_no_dates: today
+      })
+      |> Ash.create!()
+    end
+  end
+end
