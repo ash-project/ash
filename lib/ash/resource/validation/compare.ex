88c96b871a24f7da5be194fd73498c4e8a735dd5
Zach Daniel <zachary.s.daniel@gmail.com>
2025-03-16 00:07:02 -0400

    Update lib/ash/resource/validation/compare.ex

--- lib/ash/resource/validation/compare.ex
+++ lib/ash/resource/validation/compare.ex
@@ -185,7 +185,7 @@ defmodule Ash.Resource.Validation.Compare do
            )}
 
         {:is_nil, true} ->
-          {:atomic, [opts[:attribute]], expr(^atomic_ref(opts[:attribute]) != nil),
+          {:atomic, [opts[:attribute]], expr(not(is_nil(^atomic_ref(opts[:attribute])))),
            expr(
              error(^InvalidAttribute, %{
                field: ^opts[:attribute],
