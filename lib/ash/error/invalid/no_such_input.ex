451cc3b60febf22b601f6c8e302c3553a4d3f6a5
Zach Daniel <zach@zachdaniel.dev>
2025-03-27 14:20:23 -0400

    improvement: fix hint for read/generic actions in `NoSuchInput`
    closes #1919

--- lib/ash/error/invalid/no_such_input.ex
+++ lib/ash/error/invalid/no_such_input.ex
@@ -52,32 +52,38 @@ defmodule Ash.Error.Invalid.NoSuchInput do
     action = Ash.Resource.Info.action(error.resource, error.action)
 
     if action do
-      case Ash.Resource.Info.attribute(error.resource, error.input) do
-        nil ->
-          "No such attribute on #{inspect(error.resource)}, or argument on #{inspect(error.resource)}.#{error.action}"
+      if action.type in [:read, :action] do
+        """
+        Perhaps you meant to add an argument called `#{error.input}` to #{inspect(error.resource)}.#{error.action}?
+        """
+      else
+        case Ash.Resource.Info.attribute(error.resource, error.input) do
+          nil ->
+            "No such attribute on #{inspect(error.resource)}, or argument on #{inspect(error.resource)}.#{error.action}"
 
-        %{writable?: false} ->
-          """
-          The attribute exists on #{inspect(error.resource)}, but is not accepted by #{inspect(error.resource)}.#{error.action}
+          %{writable?: false} ->
+            """
+            The attribute exists on #{inspect(error.resource)}, but is not accepted by #{inspect(error.resource)}.#{error.action}
 
-          The attribute is currently `writable?: false`, which means that it can never be accepted by an action.
-          """
+            The attribute is currently `writable?: false`, which means that it can never be accepted by an action.
+            """
 
-        %{public?: false} ->
-          """
-          The attribute exists on #{inspect(error.resource)}, but is not accepted by #{inspect(error.resource)}.#{error.action}
+          %{public?: false} ->
+            """
+            The attribute exists on #{inspect(error.resource)}, but is not accepted by #{inspect(error.resource)}.#{error.action}
 
-          The attribute is currently `public?: false`, which means that it is not accepted when using `:*`, i.e in `accept :*`.
+            The attribute is currently `public?: false`, which means that it is not accepted when using `:*`, i.e in `accept :*`.
 
-          Perhaps you meant to make the attribute public, or add it to the accept list for #{inspect(error.resource)}.#{error.action}?
-          """
+            Perhaps you meant to make the attribute public, or add it to the accept list for #{inspect(error.resource)}.#{error.action}?
+            """
 
-        _attribute ->
-          """
-          The attribute exists on #{inspect(error.resource)}, but is not accepted by #{inspect(error.resource)}.#{error.action}
+          _attribute ->
+            """
+            The attribute exists on #{inspect(error.resource)}, but is not accepted by #{inspect(error.resource)}.#{error.action}
 
-          Perhaps you meant to add it to the accept list for #{inspect(error.resource)}.#{error.action}?
-          """
+            Perhaps you meant to add it to the accept list for #{inspect(error.resource)}.#{error.action}?
+            """
+        end
       end
     end
   end
