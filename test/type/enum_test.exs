a6de02a0bbcbaf0155a78930a914dc444f746d88
Chaz Watkins <chazwatkins@hey.com>
2025-03-30 07:06:00 -0500

    improvement: Ash.Type.Enum - Add optional description and label value … (#1925)
    - Add keyword list for enum values to add `description` and/or `label`
    - Add `label/1` to return the label for a given value
    - Add `details/1` to return the label and description map
    - Add validation to raise when unsupported options are provided for a value
    - Add `value_to_label/1` function that will generate a label from the `:atom` key when no label
    is provided.  For `:string` keys, the label is set to the key value.
    Example
    
    ```elixir
    defmodule MyApp.MyEnum do
      use Ash.Type.Enum, values: [
        open: "An open ticket",
        closed: [description: "A closed ticket", label: "Closed"],
        escalated: [label: "Uh oh!"]
      ]
    end
    
    ```
    
    This addition is completely backwards compatible and all existing tests pass.  Ash users that
    already have enums with descriptions, won't be affected.
    
    I attempted to switch away from the `validate_values` function and leverage constraints,
    but didn't have much luck.  Open to guidance on how to do it as I think it would be nice
    to do instead of the manual validation function.
    
    closes #1724

--- test/type/enum_test.exs
+++ test/type/enum_test.exs
@@ -78,12 +78,24 @@ defmodule Ash.Test.Type.EnumTest do
              :bar,
              :baz,
              :a_thing_with_no_description,
-             :another_thing_with_no_description
+             :another_thing_with_no_description,
+             :with_details
            ]
 
     assert DescriptiveEnum.description(:foo) == "Clearly a foo"
     assert DescriptiveEnum.description(:a_thing_with_no_description) == nil
     assert DescriptiveEnum.description(:another_thing_with_no_description) == nil
+    assert DescriptiveEnum.description(:with_details) == "Look I have a description"
+  end
+
+  test "it handles labels and generates them when no label is provided" do
+    assert DescriptiveEnum.label(:foo) == "Foo"
+    assert DescriptiveEnum.label(:a_thing_with_no_description) == "A thing with no description"
+
+    assert DescriptiveEnum.label(:another_thing_with_no_description) ==
+             "Another thing with no description"
+
+    assert DescriptiveEnum.label(:with_details) == "I have a label"
   end
 
   describe "types are correctly generated" do
@@ -102,6 +114,7 @@ defmodule Ash.Test.Type.EnumTest do
                   {:t,
                    {:type, 0, :union,
                     [
+                      {:atom, 0, :with_details},
                       {:atom, 0, :another_thing_with_no_description},
                       _,
                       _,
@@ -123,7 +136,8 @@ defmodule Ash.Test.Type.EnumTest do
                   {:t,
                    {:type, 0, :union,
                     [
-                      {:atom, 0, :foo},
+                      {:type, 0, :union,
+                       [{:atom, 0, :another_one}, {:atom, 0, :with_details}, {:atom, 0, :foo}]},
                       {:remote_type, 0, [{:atom, 0, String}, {:atom, 0, :t}, []]}
                     ]}, []}
               ]} = Code.Typespec.fetch_types(MixedEnum)
