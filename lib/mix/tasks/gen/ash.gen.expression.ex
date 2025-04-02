50f90d709d503252f3d9fc19c826b961a06222d5
ken-kost <kenneth.kostresevic@gmail.com>
2025-04-02 12:40:54 +0200

    Implement gen tasks for expression, preparation and validation

---
+++ lib/mix/tasks/gen/ash.gen.expression.ex
@@ -0,0 +1,99 @@
+if Code.ensure_loaded?(Igniter) do
+  defmodule Mix.Tasks.Ash.Gen.Expression do
+    @moduledoc """
+    Generates a custom expression
+
+    See [Ash.CustomExpression](https://hexdocs.pm/ash/Ash.CustomExpression.html) for more.
+
+    ## Example
+
+    ```bash
+    mix ash.gen.expression MyApp.Expressions.LevenshteinDistance --args string,string
+    ```
+
+    ## Options
+
+    * `--args` - A comma separated list of argument types. i.e `--args string`
+    """
+    @shortdoc "Generates a custom expression module."
+    use Igniter.Mix.Task
+
+    @impl true
+    def info(_argv, _parent) do
+      %Igniter.Mix.Task.Info{
+        positional: [:expression],
+        schema: [args: :csv, name: :string]
+      }
+    end
+
+    @impl Igniter.Mix.Task
+    def igniter(igniter) do
+      expression = igniter.args.positional.expression
+      options = igniter.args.options
+      name = name(expression, options[:name])
+
+      args =
+        case options[:args] do
+          nil -> ""
+          args -> args |> Enum.map(&":#{&1}") |> Enum.join(",")
+        end
+
+      expression = Igniter.Project.Module.parse(expression)
+
+      igniter
+      |> Igniter.Project.Module.create_module(expression, """
+      use Ash.CustomExpression,
+        name: :#{name},
+        args: [#{args}]
+
+      @impl true
+      def expression(_data_layer, args) do
+        {:ok, expr(args)}
+      end
+      """)
+    end
+
+    defp name(_expression, name) when is_binary(name), do: name
+
+    defp name(expression, _name) do
+      expression
+      |> String.split(".")
+      |> List.last()
+      |> then(fn <<first::utf8, rest::binary>> ->
+        String.downcase(<<first::utf8>>) <>
+          (rest
+           |> String.split(~r/(?=[A-Z])/)
+           |> Enum.map(&String.downcase(&1))
+           |> Enum.join("_"))
+      end)
+    end
+  end
+else
+  defmodule Mix.Tasks.Ash.Gen.Expression do
+    @moduledoc """
+    Generates a custom expression
+
+    See [Ash.CustomExpression](https://hexdocs.pm/ash/Ash.CustomExpression.html) for more.
+
+    ## Example
+
+    ```bash
+    mix ash.gen.expression MyApp.Expressions.LevenshteinDistance --args string,string
+    ```
+    """
+    @shortdoc "Generates a custom expression module."
+    use Mix.Task
+
+    def run(_argv) do
+      Mix.shell().error("""
+      The task 'ash.gen.expression' requires igniter to be run.
+
+      Please install igniter and try again.
+
+      For more information, see: https://hexdocs.pm/igniter
+      """)
+
+      exit({:shutdown, 1})
+    end
+  end
+end
---
+++ lib/mix/tasks/gen/ash.gen.preparation.ex
@@ -0,0 +1,73 @@
+if Code.ensure_loaded?(Igniter) do
+  defmodule Mix.Tasks.Ash.Gen.Preparation do
+    @moduledoc """
+    Generates a custom preparation
+
+    See [Custom Preparations](https://hexdocs.pm/ash/preparations.html#custom-preparations) for more.
+
+    ## Example
+
+    ```bash
+    mix ash.gen.preparation MyApp.Preparations.Top5
+    ```
+    """
+    @shortdoc "Generates a custom preparation module."
+    use Igniter.Mix.Task
+
+    @impl true
+    def info(_argv, _parent) do
+      %Igniter.Mix.Task.Info{
+        positional: [:preparation]
+      }
+    end
+
+    @impl Igniter.Mix.Task
+    def igniter(igniter) do
+      preparation = igniter.args.positional.preparation
+      preparation = Igniter.Project.Module.parse(preparation)
+
+      igniter
+      |> Igniter.Project.Module.create_module(preparation, """
+      use Ash.Resource.Preparation
+
+      @impl true
+      def init(opts) do
+        {:ok, opts}
+      end
+
+      @impl true
+      def prepare(query, _opts, _context) do
+        query
+      end
+      """)
+    end
+  end
+else
+  defmodule Mix.Tasks.Ash.Gen.Preparation do
+    @moduledoc """
+    Generates a custom preparation
+
+    See [Custom Preparations](https://hexdocs.pm/ash/preparations.html#custom-preparations) for more.
+
+    ## Example
+
+    ```bash
+    mix ash.gen.preparation MyApp.Preparations.Top5
+    ```
+    """
+    @shortdoc "Generates a custom preparation module."
+    use Mix.Task
+
+    def run(_argv) do
+      Mix.shell().error("""
+      The task 'ash.gen.preparation' requires igniter to be run.
+
+      Please install igniter and try again.
+
+      For more information, see: https://hexdocs.pm/igniter
+      """)
+
+      exit({:shutdown, 1})
+    end
+  end
+end
