05b59350851d14f8fc4881e5a0e7702066d23d6b
Brian Stubbs <stentroad@gmail.com>
2025-04-09 15:01:40 -0300

    test: start_of_day correctly handles timezone

--- test/query/function/start_of_day_test.exs
+++ test/query/function/start_of_day_test.exs
@@ -12,4 +12,19 @@ defmodule Ash.Query.Function.StartOfDayTest do
     assert StartOfDay.evaluate(%{arguments: [DateTime.utc_now()]}) ==
              {:known, DateTime.to_date(DateTime.utc_now()) |> DateTime.new!(Time.new!(0, 0, 0))}
   end
+
+  @nz_tz "Pacific/Auckland"
+
+  test "timezone handled correctly" do
+    utc = ~U[2025-04-09 19:00:00Z]
+    assert DateTime.to_date(utc) == ~D[2025-04-09]
+    nz = DateTime.shift_zone!(utc, @nz_tz)
+    # The 10th in New Zealand!
+    assert DateTime.to_date(nz) == ~D[2025-04-10]
+    expected_start_of_day_nz = DateTime.new!(~D[2025-04-10], ~T[00:00:00], @nz_tz)
+    {:known, start_of_day_utc} = StartOfDay.evaluate(%{arguments: [utc, @nz_tz]})
+
+    assert DateTime.shift_zone!(start_of_day_utc, @nz_tz) ==
+             expected_start_of_day_nz
+  end
 end
