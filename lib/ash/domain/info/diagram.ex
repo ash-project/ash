555c763378f925aaebf8dea2ec3148cb22a144ae
Zach Daniel <zach@zachdaniel.dev>
2025-04-30 11:32:21 -0400

    improvement: add `Ash.Info` for general application information

--- lib/ash/domain/info/diagram.ex
+++ lib/ash/domain/info/diagram.ex
@@ -17,23 +17,45 @@ defmodule Ash.Domain.Info.Diagram do
 
   @default_opts indent: @indent, show_private?: @show_private?
 
-  defp resource_name(resource) do
-    resource
-    |> Ash.Resource.Info.short_name()
-    |> to_string()
-    |> Macro.camelize()
+  defp resource_name(resource, opts) do
+    if opts[:name] == :full do
+      inspect(resource)
+    else
+      resource
+      |> Ash.Resource.Info.short_name()
+      |> to_string()
+      |> Macro.camelize()
+    end
   end
 
-  defp short_module(module) do
-    module
-    |> Module.split()
-    |> List.last()
+  defp domain_name(domain, opts) do
+    if opts[:name] == :full do
+      inspect(domain)
+    else
+      domain
+      |> Ash.Domain.Info.short_name()
+      |> to_string()
+      |> Macro.camelize()
+    end
+  end
+
+  defp short_module(module, opts) do
+    if opts[:name] == :full do
+      inspect(module)
+    else
+      module
+      |> Module.split()
+      |> List.last()
+    end
   end
 
   defp normalise_relationships(domain) do
     for resource <- Ash.Domain.Info.resources(domain) do
       for relationship <- Ash.Resource.Info.relationships(resource) do
-        [relationship.source, relationship.destination]
+        [
+          relationship.source,
+          relationship.destination
+        ]
         |> Enum.sort()
         |> List.to_tuple()
       end
@@ -43,11 +65,11 @@ defmodule Ash.Domain.Info.Diagram do
     |> Enum.sort()
   end
 
-  defp aggregate_type(_resource, %{kind: :custom, type: type}) do
-    short_type(type)
+  defp aggregate_type(_resource, %{kind: :custom, type: type}, opts) do
+    short_type(type, opts)
   end
 
-  defp aggregate_type(resource, aggregate) do
+  defp aggregate_type(resource, aggregate, opts) do
     attribute =
       if aggregate.field do
         related = Ash.Resource.Info.related(resource, aggregate.relationship_path)
@@ -67,23 +89,87 @@ defmodule Ash.Domain.Info.Diagram do
     {:ok, type, _constraints} =
       Ash.Query.Aggregate.kind_to_type(aggregate.kind, attribute_type, attribute_constraints)
 
-    short_type(type)
+    short_type(type, opts)
   end
 
   # default to one to one to just show connection
   defp rel_type, do: "||--||"
 
-  defp short_type({:array, t}), do: "ArrayOf#{short_module(t)}"
-  defp short_type(t), do: short_module(t)
+  defp short_type({:array, t}, opts), do: "ArrayOf#{short_type(t, opts)}"
+  defp short_type(t, opts), do: short_module(t, opts)
 
   @doc """
-  Generates a Mermaid Entity Relationship Diagram for a given domain.
+  Generates a Mermaid Entity Relationship Diagram for a given domain or list of domains.
 
   Shows only public attributes, calculations, aggregates and actions.
   Shows a one-to-one line for relationships as enumerating all unidirectional
   relationships is far too noisy.
   """
-  def mermaid_er_diagram(domain, opts \\ @default_opts) do
+
+  def mermaid_er_diagram(domain, opts \\ @default_opts)
+
+  def mermaid_er_diagram(domains, opts) when is_list(domains) do
+    indent = opts[:indent] || @indent
+    show_private? = Access.get(opts, :show_private?, @show_private?)
+
+    resources =
+      for domain <- domains do
+        for resource <- Ash.Domain.Info.resources(domain) do
+          {attrs, calcs, aggs} =
+            if show_private? do
+              {
+                Ash.Resource.Info.attributes(resource),
+                Ash.Resource.Info.calculations(resource),
+                Ash.Resource.Info.aggregates(resource)
+              }
+            else
+              {
+                Ash.Resource.Info.public_attributes(resource),
+                Ash.Resource.Info.public_calculations(resource),
+                Ash.Resource.Info.public_aggregates(resource)
+              }
+            end
+
+          contents =
+            [
+              join_template(attrs, indent, &"#{short_type(&1.type, opts)} #{&1.name}"),
+              join_template(calcs, indent, &"#{short_type(&1.type, opts)} #{&1.name}"),
+              join_template(aggs, indent, &"#{aggregate_type(resource, &1, opts)} #{&1.name}")
+            ]
+            |> Enum.reject(&(&1 == ""))
+            |> Enum.join("\n")
+
+          """
+          #{indent}#{resource_name(resource, opts)} {
+          #{indent}#{indent}Domain: #{domain_name(domain, opts)}#{source_link(resource, indent, opts)}
+          #{contents}
+          #{indent}}
+          """
+        end
+        |> Enum.join()
+      end
+
+    relationships =
+      for domain <- domains,
+          {src, dest} <- normalise_relationships(domain) do
+        ~s(#{indent}#{resource_name(src, opts)} #{rel_type()} #{resource_name(dest, opts)} : "")
+      end
+      |> Enum.join("\n")
+
+    domains =
+      Enum.map_join(domains, "\n", fn domain ->
+        ~s(#{indent}#{domain_name(domain, opts)})
+      end)
+
+    """
+    erDiagram
+    #{domains}
+    #{resources}
+    #{relationships}
+    """
+  end
+
+  def mermaid_er_diagram(domain, opts) do
     indent = opts[:indent] || @indent
     show_private? = Access.get(opts, :show_private?, @show_private?)
 
@@ -106,15 +192,15 @@ defmodule Ash.Domain.Info.Diagram do
 
         contents =
           [
-            join_template(attrs, indent, &"#{short_type(&1.type)} #{&1.name}"),
-            join_template(calcs, indent, &"#{short_type(&1.type)} #{&1.name}"),
-            join_template(aggs, indent, &"#{aggregate_type(resource, &1)} #{&1.name}")
+            join_template(attrs, indent, &"#{short_type(&1.type, opts)} #{&1.name}"),
+            join_template(calcs, indent, &"#{short_type(&1.type, opts)} #{&1.name}"),
+            join_template(aggs, indent, &"#{aggregate_type(resource, &1, opts)} #{&1.name}")
           ]
           |> Enum.reject(&(&1 == ""))
           |> Enum.join("\n")
 
         """
-        #{indent}#{resource_name(resource)} {
+        #{indent}#{resource_name(resource, opts)} {
         #{contents}
         #{indent}}
         """
@@ -123,7 +209,7 @@ defmodule Ash.Domain.Info.Diagram do
 
     relationships =
       for {src, dest} <- normalise_relationships(domain) do
-        ~s(#{indent}#{resource_name(src)} #{rel_type()} #{resource_name(dest)} : "")
+        ~s(#{indent}#{resource_name(src, opts)} #{rel_type()} #{resource_name(dest, opts)} : "")
       end
       |> Enum.join("\n")
 
@@ -134,14 +220,14 @@ defmodule Ash.Domain.Info.Diagram do
     """
   end
 
-  defp class_short_type({:array, t}), do: "#{short_module(t)}[]"
-  defp class_short_type(t), do: short_module(t)
+  defp class_short_type({:array, t}, opts), do: "#{short_module(t, opts)}[]"
+  defp class_short_type(t, opts), do: short_module(t, opts)
 
   defp join_template(list, indent, template_fn) do
     Enum.map_join(list, "\n", fn item -> "#{indent}#{indent}#{template_fn.(item)}" end)
   end
 
-  defp list_arguments_and_attributes(resource, action) do
+  defp list_arguments_and_attributes(resource, action, opts) do
     arguments = action.arguments
 
     attributes =
@@ -152,17 +238,17 @@ defmodule Ash.Domain.Info.Diagram do
         %{accept: accept} -> accept |> Enum.map(&Ash.Resource.Info.attribute(resource, &1))
       end
 
-    list_arguments(Enum.uniq_by(arguments ++ attributes, & &1.name))
+    list_arguments(Enum.uniq_by(arguments ++ attributes, & &1.name), opts)
   end
 
-  defp list_arguments(arguments) when length(arguments) > @argument_print_limit do
+  defp list_arguments(arguments, opts) when length(arguments) > @argument_print_limit do
     {displayed_args, _rest} = Enum.split(arguments, @argument_print_limit)
-    list_arguments(displayed_args) <> ", ..."
+    list_arguments(displayed_args, opts) <> ", ..."
   end
 
-  defp list_arguments(arguments) do
+  defp list_arguments(arguments, opts) do
     arguments
-    |> Enum.map_join(", ", &"#{class_short_type(&1.type)} #{&1.name}")
+    |> Enum.map_join(", ", &"#{class_short_type(&1.type, opts)} #{&1.name}")
   end
 
   @doc """
@@ -172,7 +258,80 @@ defmodule Ash.Domain.Info.Diagram do
   Shows a connecting line for relationships with the type of relationship
   indicated in the attribute list.
   """
-  def mermaid_class_diagram(domain, opts \\ @default_opts) do
+  def mermaid_class_diagram(domain, opts \\ @default_opts)
+
+  def mermaid_class_diagram(domains, opts) when is_list(domains) do
+    indent = opts[:indent] || @indent
+    show_private? = Access.get(opts, :show_private?, @show_private?)
+
+    resources =
+      for domain <- domains, resource <- Ash.Domain.Info.resources(domain) do
+        actions = Ash.Resource.Info.actions(resource)
+
+        {attrs, calcs, aggs, relationships} =
+          if show_private? do
+            {
+              Ash.Resource.Info.attributes(resource),
+              Ash.Resource.Info.calculations(resource),
+              Ash.Resource.Info.aggregates(resource),
+              Ash.Resource.Info.relationships(resource)
+            }
+          else
+            {Ash.Resource.Info.public_attributes(resource),
+             Ash.Resource.Info.public_calculations(resource),
+             Ash.Resource.Info.public_aggregates(resource),
+             Ash.Resource.Info.public_relationships(resource)}
+          end
+
+        contents =
+          [
+            join_template(attrs, indent, &"#{class_short_type(&1.type, opts)} #{&1.name}"),
+            join_template(calcs, indent, &"#{class_short_type(&1.type, opts)} #{&1.name}"),
+            join_template(aggs, indent, &"#{aggregate_type(resource, &1, opts)} #{&1.name}"),
+            join_template(
+              relationships,
+              indent,
+              &"#{resource_name(&1.destination, opts)}#{if &1.cardinality == :many, do: "[]", else: ""} #{&1.name}"
+            ),
+            join_template(
+              actions,
+              indent,
+              &"#{&1.name}(#{list_arguments_and_attributes(resource, &1, opts)})"
+            )
+          ]
+          |> Enum.reject(&(&1 == ""))
+          |> Enum.join("\n")
+
+        """
+        #{indent}class #{resource_name(resource, opts)} {
+        #{indent}#{indent}Domain: #{domain_name(domain, opts)}#{source_link(resource, indent, opts)}
+        #{contents}
+        #{indent}}
+        """
+      end
+      |> Enum.join()
+
+    relationships =
+      for domain <- domains,
+          {src, dest} <- normalise_relationships(domain) do
+        ~s(#{indent}#{resource_name(src, opts)} -- #{resource_name(dest, opts)})
+      end
+      |> Enum.join("\n")
+
+    domains =
+      Enum.map_join(domains, "\n", fn domain ->
+        ~s(#{indent}#{domain_name(domain, opts)})
+      end)
+
+    """
+    classDiagram
+    #{domains}
+    #{resources}
+    #{relationships}
+    """
+  end
+
+  def mermaid_class_diagram(domain, opts) do
     indent = opts[:indent] || @indent
     show_private? = Access.get(opts, :show_private?, @show_private?)
 
@@ -197,25 +356,25 @@ defmodule Ash.Domain.Info.Diagram do
 
         contents =
           [
-            join_template(attrs, indent, &"#{class_short_type(&1.type)} #{&1.name}"),
-            join_template(calcs, indent, &"#{class_short_type(&1.type)} #{&1.name}"),
-            join_template(aggs, indent, &"#{aggregate_type(resource, &1)} #{&1.name}"),
+            join_template(attrs, indent, &"#{class_short_type(&1.type, opts)} #{&1.name}"),
+            join_template(calcs, indent, &"#{class_short_type(&1.type, opts)} #{&1.name}"),
+            join_template(aggs, indent, &"#{aggregate_type(resource, &1, opts)} #{&1.name}"),
             join_template(
               relationships,
               indent,
-              &"#{resource_name(&1.destination)}#{if &1.cardinality == :many, do: "[]", else: ""} #{&1.name}"
+              &"#{resource_name(&1.destination, opts)}#{if &1.cardinality == :many, do: "[]", else: ""} #{&1.name}"
             ),
             join_template(
               actions,
               indent,
-              &"#{&1.name}(#{list_arguments_and_attributes(resource, &1)})"
+              &"#{&1.name}(#{list_arguments_and_attributes(resource, &1, opts)})"
             )
           ]
           |> Enum.reject(&(&1 == ""))
           |> Enum.join("\n")
 
         """
-        #{indent}class #{resource_name(resource)} {
+        #{indent}class #{resource_name(resource, opts)} {
         #{contents}
         #{indent}}
         """
@@ -224,7 +383,7 @@ defmodule Ash.Domain.Info.Diagram do
 
     relationships =
       for {src, dest} <- normalise_relationships(domain) do
-        ~s(#{indent}#{resource_name(src)} -- #{resource_name(dest)})
+        ~s(#{indent}#{resource_name(src, opts)} -- #{resource_name(dest, opts)})
       end
       |> Enum.join("\n")
 
@@ -233,5 +392,17 @@ defmodule Ash.Domain.Info.Diagram do
     #{resources}
     #{relationships}
     """
+  end
+
+  defp source_link(module, indent, opts) do
+    if opts[:include_source_links?] do
+      module.module_info(:compile)[:source]
+      |> List.to_string()
+      |> Path.relative_to_cwd()
+      |> then(&"\n#{indent}#{indent}Source: #{&1}\n")
+    end
+  rescue
+    _ ->
+      nil
   end
 end
---
+++ lib/ash/info.ex
@@ -0,0 +1,53 @@
+defmodule Ash.Info do
+  @moduledoc """
+  General introspection helpers for Ash applications.
+
+  Use `Ash.Info.mermaid_overview(otp_app)` to generate a mermaid chart of the application's domains and resources.
+  This offers a high level view, but will not include information from extensions. Use `resource_report/2`
+  for a detailed report of a resource.
+
+  Use `Ash.Info.mermaid_overview(otp_app, type: :entity_relationship)` to get a simplified entity relationship diagram.
+
+  Use `Ash.Info.domains_and_resources(otp_app)` to get a map where the keys are the domains, and the value is the list
+  of resources for that domain.
+  """
+
+  @spec domains(atom()) :: [Ash.Domain.t()]
+  def domains(otp_app) do
+    Application.get_env(otp_app, :ash_domains, [])
+  end
+
+  @doc """
+  Returns a map where the keys are the domains, and the value is the list of resources for that domain.
+  """
+  @spec domains_and_resources(atom()) :: %{Ash.Domain.t() => [Ash.Resource.t()]}
+  def domains_and_resources(otp_app) do
+    otp_app
+    |> Application.get_env(:ash_domains, [])
+    |> Map.new(&{&1, Ash.Domain.Info.resources(&1)})
+  end
+
+  @doc """
+  Generate a mermaid chart of the application's domains and resources.
+
+  This offers a high level view, but will not include information from extensions.
+  Use `resource_report/2` for a detailed report of a resource.
+  """
+  def mermaid_overview(otp_app, type \\ :class) do
+    case type do
+      :class ->
+        Ash.Domain.Info.Diagram.mermaid_class_diagram(domains(otp_app),
+          show_private?: true,
+          name: :full,
+          include_source_links?: true
+        )
+
+      :entity_relationship ->
+        Ash.Domain.Info.Diagram.mermaid_er_diagram(domains(otp_app),
+          show_private?: true,
+          name: :full,
+          include_source_links?: true
+        )
+    end
+  end
+end
