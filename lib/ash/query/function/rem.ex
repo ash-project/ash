3e839b828b26edd9c6c69686e293598a99ab3c75
Kenneth Kostrešević <kenneth.kostresevic@gmail.com>
2025-04-30 18:49:36 +0200

    improvement: add rem/2 expr (#2004)

---
+++ lib/ash/query/function/rem.ex
@@ -0,0 +1,17 @@
+defmodule Ash.Query.Function.Rem do
+  @moduledoc """
+  Rounds a float, decimal or integer to the given number of points
+  """
+
+  use Ash.Query.Function, name: :rem
+
+  def args,
+    do: [[:integer, :integer]]
+
+  def returns, do: [:integer]
+
+  def evaluate(%{arguments: [num1, num2]}) when is_integer(num1) and is_integer(num2),
+    do: {:known, rem(num1, num2)}
+
+  def can_return_nil?(_), do: false
+end
