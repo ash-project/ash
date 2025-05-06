b985c8a71eb2596b5b910dd0281d402b053e25d8
Zach Daniel <zach@zachdaniel.dev>
2025-05-05 22:44:30 -0400

    docs: add typespecs to Ash.UUID

--- lib/ash/uuid.ex
+++ lib/ash/uuid.ex
@@ -1,6 +1,16 @@
 defmodule Ash.UUID do
   @moduledoc "Helpers for working with UUIDs"
 
+  @typedoc """
+  A hex-encoded UUID string.
+  """
+  @type t :: Ecto.UUID.t()
+
+  @typedoc """
+  A raw binary representation of a UUID.
+  """
+  @type raw :: Ecto.UUID.raw()
+
   @doc "Generates a new uuid"
   @spec generate() :: Ecto.UUID.t()
   def generate do
