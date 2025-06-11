b217c0ef36c467230c4194d51721ec5a5da55249
Zach Daniel <zach@zachdaniel.dev>
2025-06-11 16:03:14 -0400

    chore: fix test matching

--- test/type/new_type_test.exs
+++ test/type/new_type_test.exs
@@ -5,7 +5,7 @@ defmodule Ash.Test.Type.NewTypeTest do
   defmodule SSN do
     use Ash.Type.NewType,
       subtype_of: :string,
-      constraints: [match: ~r/^(?!0{3})(?!6{3})[0-8]\\d{2}-(?!0{2})\\d{2}-(?!0{4})\\d{4}$/]
+      constraints: [match: ~r/^(?!0{3})(?!6{3})[0-8]\d{2}-(?!0{2})\d{2}-(?!0{4})\d{4}$/]
   end
 
   defmodule CustomMap do
