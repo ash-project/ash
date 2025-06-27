459111273a731b8f4505707991f48037f9dc6603
Igor Barakaiev <ibarakaiev@gmail.com>
2025-06-27 07:10:25 -0700

    test: add tests for updates with argument-based changes (#2168)

--- test/resource/actions/update_test.exs
+++ test/resource/actions/update_test.exs
@@ -74,4 +74,74 @@ defmodule Ash.Test.Dsl.Resource.Actions.UpdateTest do
       )
     end
   end
+
+  describe "updates with argument-based changes" do
+    test "atomic_update should work with argument expressions without requiring attribute in accept list" do
+      defmodule Post1 do
+        @moduledoc false
+        use Ash.Resource,
+          domain: Domain,
+          data_layer: Ash.DataLayer.Ets
+
+        attributes do
+          uuid_primary_key :id
+          attribute :oban_job_id, :integer
+        end
+
+        actions do
+          defaults [:read, create: []]
+
+          update :set_oban_job_id do
+            argument :job_id, :integer, allow_nil?: false
+
+            change atomic_update(:oban_job_id, expr(^arg(:job_id)))
+          end
+        end
+
+        code_interface do
+          define :create, action: :create
+          define :set_oban_job_id, action: :set_oban_job_id, args: [:job_id]
+        end
+      end
+
+      {:ok, post} = Post1.create()
+
+      assert {:ok, updated_post} = Post1.set_oban_job_id(post, 123)
+      assert updated_post.oban_job_id == 123
+    end
+
+    test "set_attribute should work with argument expressions without requiring attribute in accept list" do
+      defmodule Post2 do
+        @moduledoc false
+        use Ash.Resource,
+          domain: Domain,
+          data_layer: Ash.DataLayer.Ets
+
+        attributes do
+          uuid_primary_key :id
+          attribute :oban_job_id, :integer
+        end
+
+        actions do
+          defaults [:read, create: []]
+
+          update :set_oban_job_id do
+            argument :job_id, :integer, allow_nil?: false
+
+            change set_attribute(:oban_job_id, arg(:job_id))
+          end
+        end
+
+        code_interface do
+          define :create, action: :create
+          define :set_oban_job_id, action: :set_oban_job_id, args: [:job_id]
+        end
+      end
+
+      {:ok, post} = Post2.create()
+
+      assert {:ok, updated_post} = Post2.set_oban_job_id(post, 123)
+      assert updated_post.oban_job_id == 123
+    end
+  end
 end
