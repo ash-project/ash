47922c67d68b242bf11a008319ccdcd24df0f65b
Chaz Watkins <chazwatkins@hey.com>
2025-07-21 12:13:22 -0500

    chore: remove run_before_actions/run_after_actions from Ash.Subject

--- lib/ash/subject.ex
+++ lib/ash/subject.ex
@@ -356,44 +356,4 @@ defmodule Ash.Subject do
       %{subject | after_action: subject.after_action ++ [callback]}
     end
   end
-
-  @doc """
-  Executes all before_action callbacks on the subject.
-
-  Only supported by Changeset and ActionInput.
-
-  ## Parameters
-
-    * `subject` - The subject to run callbacks on
-  """
-  @spec run_before_actions(Ash.Changeset.t() | Ash.ActionInput.t()) ::
-          {Ash.Changeset.t() | Ash.ActionInput.t(), map()}
-  def run_before_actions(%Ash.Changeset{} = subject) do
-    Ash.Changeset.run_before_actions(subject)
-  end
-
-  def run_before_actions(%Ash.ActionInput{} = subject) do
-    Ash.ActionInput.run_before_actions(subject)
-  end
-
-  @doc """
-  Executes all after_action callbacks on the subject.
-
-  Only supported by Changeset and ActionInput.
-
-  ## Parameters
-
-    * `result` - The result data from the action
-    * `subject` - The subject that was executed
-    * `notifications` - Notifications from before_action hooks
-  """
-  @spec run_after_actions(term(), Ash.Changeset.t() | Ash.ActionInput.t(), term()) ::
-          {term(), list()}
-  def run_after_actions(result, %Ash.Changeset{} = subject, before_action_notifications) do
-    Ash.Changeset.run_after_actions(result, subject, before_action_notifications)
-  end
-
-  def run_after_actions(result, %Ash.ActionInput{} = subject, before_action_notifications) do
-    Ash.ActionInput.run_after_actions(result, subject, before_action_notifications)
-  end
 end
