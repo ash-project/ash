202ffae7ffdeea30b0450a7f5fe8b6dfdc7f97c9
Abhishek Tripathi <abhishek.tripathi456@gmail.com>
2025-08-07 08:06:23 +0530

    fix: handle multiple InvalidAttribute errors

--- lib/ash/typed_struct.ex
+++ lib/ash/typed_struct.ex
@@ -218,16 +218,36 @@ defmodule Ash.TypedStruct do
                    {:ok, value}
 
                  {:error, error} ->
-                   if Keyword.keyword?(error) do
-                     {:error,
-                      Ash.Error.Changes.InvalidAttribute.exception(
-                        field: error[:field],
-                        message: error[:message],
-                        value: error[:value],
-                        vars: error
-                      )}
-                   else
-                     {:error, Ash.Error.to_ash_error(error)}
+                   cond do
+                     Keyword.keyword?(error) ->
+                       {:error,
+                        Ash.Error.Changes.InvalidAttribute.exception(
+                          field: error[:field],
+                          message: error[:message],
+                          value: error[:value],
+                          vars: error
+                        )}
+
+                     is_list(error) && Enum.all?(error, &Keyword.keyword?/1) ->
+                       # Handle list of keyword errors (from struct validation)
+                       errors =
+                         Enum.map(error, fn err ->
+                           Ash.Error.Changes.InvalidAttribute.exception(
+                             field: err[:field],
+                             message: err[:message],
+                             value: err[:value],
+                             vars: err
+                           )
+                         end)
+
+                       # Return single error for backward compatibility if only one error
+                       case errors do
+                         [single_error] -> {:error, single_error}
+                         multiple_errors -> {:error, multiple_errors}
+                       end
+
+                     true ->
+                       {:error, Ash.Error.to_ash_error(error)}
                    end
                end
              end
