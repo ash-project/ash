65c7b767f875cf7b5e1d36034c5d43fd0355aef4
Zach Daniel <zach@zachdaniel.dev>
2025-09-25 00:05:26 -0400

    improvement: add location info for unique action name transformer

--- lib/ash/resource/transformers/require_unique_action_names.ex
+++ lib/ash/resource/transformers/require_unique_action_names.ex
@@ -5,15 +5,22 @@ defmodule Ash.Resource.Transformers.RequireUniqueActionNames do
   use Spark.Dsl.Transformer
 
   alias Spark.Dsl.Transformer
+  alias Spark.Dsl.Extension
+  alias Spark.Dsl.Entity
   alias Spark.Error.DslError
 
   def transform(dsl_state) do
     dsl_state
-    |> Transformer.get_entities([:actions])
+    |> Ash.Resource.Info.actions()
     |> Enum.group_by(& &1.name)
     |> Enum.each(fn {name, actions} ->
       if Enum.count(actions) != 1 do
+        # Find the first action with this name to get location info
+        second_action = Enum.at(actions, 1)
+        location = Entity.anno(second_action)
+
         raise DslError.exception(
+                module: Transformer.get_persisted(dsl_state, :module),
                 message: """
                 Multiple actions (#{Enum.count(actions)}) with the name `#{name}` defined.
 
@@ -21,7 +28,8 @@ defmodule Ash.Resource.Transformers.RequireUniqueActionNames do
                 was promoted in the documentation, but that is no longer valid. All
                 actions must have unique names.
                 """,
-                path: [:actions, name]
+                path: [:actions, name],
+                location: location
               )
       end
     end)
