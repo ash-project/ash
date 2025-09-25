98a16191c54fe2b4e837eabf290949d15f0c7aba
Zach Daniel <zach@zachdaniel.dev>
2025-09-25 00:08:12 -0400

    improvement: add location for multitenancy verifier

--- lib/ash/resource/verifiers/validate_multitenancy.ex
+++ lib/ash/resource/verifiers/validate_multitenancy.ex
@@ -5,6 +5,8 @@ defmodule Ash.Resource.Verifiers.ValidateMultitenancy do
   use Spark.Dsl.Verifier
 
   alias Spark.Dsl.Verifier
+  alias Spark.Dsl.Extension
+  alias Spark.Error.DslError
 
   # sobelow_skip ["DOS.BinToAtom"]
   @impl true
@@ -17,17 +19,27 @@ defmodule Ash.Resource.Verifiers.ValidateMultitenancy do
 
     cond do
       strategy == :context && not data_layer.can?(resource, :multitenancy) ->
+        # Get location info for the multitenancy strategy option
+        strategy_anno = Extension.get_opt_anno(dsl_state, [:multitenancy], :strategy)
+
         {:error,
-         Spark.Error.DslError.exception(
+         DslError.exception(
+           module: resource,
            path: [:multitenancy, :strategy],
-           message: "Data layer does not support multitenancy"
+           message: "Data layer does not support multitenancy",
+           location: strategy_anno
          )}
 
       strategy == :attribute && !Enum.any?(attributes, &(&1.name == attribute)) ->
+        # Get location info for the multitenancy attribute option
+        attribute_anno = Extension.get_opt_anno(dsl_state, [:multitenancy], :attribute)
+
         {:error,
-         Spark.Error.DslError.exception(
+         DslError.exception(
+           module: resource,
            path: [:multitenancy, :attribute],
-           message: "Attribute #{attribute} used in multitenancy configuration does not exist"
+           message: "Attribute #{attribute} used in multitenancy configuration does not exist",
+           location: attribute_anno
          )}
 
       true ->
