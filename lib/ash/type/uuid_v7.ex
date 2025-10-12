751d8a4d11564efbe358c90c8347f3a00eb60782
Torkild Gundersen Kjevik <torkild.kjevik@boitano.no>
2025-10-12 13:37:32 +0200

    Fix cast input for uuids (#2373)
    * fix: validate UUID structure to reject arbitrary 16-byte
      strings
    
    * chore: add coerce for Ash.Type.UUID & Ash.Type.UUIDv7
    
    * fix: add encode/1 to Ash.Type.UUIDv7
    
    * test: add tests for coerce/2 in Ash.Type.UUID & Ash.Type.UUIDv7
    
    * refactor: move pattern matching to function headers.

--- lib/ash/type/uuid_v7.ex
+++ lib/ash/type/uuid_v7.ex
@@ -25,24 +25,42 @@ defmodule Ash.Type.UUIDv7 do
   end
 
   @impl true
-  def cast_input(%Ash.CiString{string: string}, constraints), do: cast_input(string, constraints)
-  def cast_input(nil, _), do: {:ok, nil}
-
-  def cast_input(
-        <<a1, a2, a3, a4, a5, a6, a7, a8, ?-, b1, b2, b3, b4, ?-, c1, c2, c3, c4, ?-, d1, d2, d3,
-          d4, ?-, e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12>>,
-        _
-      ) do
-    <<c(a1), c(a2), c(a3), c(a4), c(a5), c(a6), c(a7), c(a8), ?-, c(b1), c(b2), c(b3), c(b4), ?-,
-      c(c1), c(c2), c(c3), c(c4), ?-, c(d1), c(d2), c(d3), c(d4), ?-, c(e1), c(e2), c(e3), c(e4),
-      c(e5), c(e6), c(e7), c(e8), c(e9), c(e10), c(e11), c(e12)>>
-  catch
-    :error -> :error
-  else
-    hex_uuid -> {:ok, hex_uuid}
+  def coerce(<<_::128>> = value, _) do
+    {:ok, encode(value)}
   end
 
-  def cast_input(<<_::128>> = value, _), do: {:ok, Ash.UUIDv7.encode(value)}
+  def coerce(value, _) do
+    cast_input(value, [])
+  end
+
+  @impl true
+  def cast_input(nil, _), do: {:ok, nil}
+
+  def cast_input(%Ash.CiString{string: string}, constraints), do: cast_input(string, constraints)
+
+  def cast_input(<<_::48, 7::4, _::12, 0b10::2, _::62>> = value, _) do
+    {:ok, Ash.UUIDv7.encode(value)}
+  end
+
+  def cast_input(value, _) when is_binary(value) do
+    case String.trim(value) do
+      <<a1, a2, a3, a4, a5, a6, a7, a8, ?-, b1, b2, b3, b4, ?-, c1, c2, c3, c4, ?-, d1, d2, d3,
+        d4, ?-, e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12>> ->
+        try do
+          <<c(a1), c(a2), c(a3), c(a4), c(a5), c(a6), c(a7), c(a8), ?-, c(b1), c(b2), c(b3),
+            c(b4), ?-, c(c1), c(c2), c(c3), c(c4), ?-, c(d1), c(d2), c(d3), c(d4), ?-, c(e1),
+            c(e2), c(e3), c(e4), c(e5), c(e6), c(e7), c(e8), c(e9), c(e10), c(e11), c(e12)>>
+        catch
+          :error -> :error
+        else
+          hex_uuid -> {:ok, hex_uuid}
+        end
+
+      _ ->
+        :error
+    end
+  end
+
   def cast_input(_, _), do: :error
 
   @impl true
@@ -92,4 +110,33 @@ defmodule Ash.Type.UUIDv7 do
   defp c(?e), do: ?e
   defp c(?f), do: ?f
   defp c(_), do: throw(:error)
+
+  defp encode(
+         <<a1::4, a2::4, a3::4, a4::4, a5::4, a6::4, a7::4, a8::4, b1::4, b2::4, b3::4, b4::4,
+           c1::4, c2::4, c3::4, c4::4, d1::4, d2::4, d3::4, d4::4, e1::4, e2::4, e3::4, e4::4,
+           e5::4, e6::4, e7::4, e8::4, e9::4, e10::4, e11::4, e12::4>>
+       ) do
+    <<e(a1), e(a2), e(a3), e(a4), e(a5), e(a6), e(a7), e(a8), ?-, e(b1), e(b2), e(b3), e(b4), ?-,
+      e(c1), e(c2), e(c3), e(c4), ?-, e(d1), e(d2), e(d3), e(d4), ?-, e(e1), e(e2), e(e3), e(e4),
+      e(e5), e(e6), e(e7), e(e8), e(e9), e(e10), e(e11), e(e12)>>
+  end
+
+  @compile {:inline, e: 1}
+
+  defp e(0), do: ?0
+  defp e(1), do: ?1
+  defp e(2), do: ?2
+  defp e(3), do: ?3
+  defp e(4), do: ?4
+  defp e(5), do: ?5
+  defp e(6), do: ?6
+  defp e(7), do: ?7
+  defp e(8), do: ?8
+  defp e(9), do: ?9
+  defp e(10), do: ?a
+  defp e(11), do: ?b
+  defp e(12), do: ?c
+  defp e(13), do: ?d
+  defp e(14), do: ?e
+  defp e(15), do: ?f
 end
