fed7f2169f805d62d96a096a279fb6e36f1adedf
ken-kost <kenneth.kostresevic@gmail.com>
2025-12-15 16:44:20 +0100

    enhancement: on setup check db connectivity

--- lib/mix/tasks/ash.setup.ex
+++ lib/mix/tasks/ash.setup.ex
@@ -12,9 +12,11 @@ defmodule Mix.Tasks.Ash.Setup do
   @doc @shortdoc
   def run(argv) do
     Mix.Task.run("compile")
+    extensions = Ash.Mix.Tasks.Helpers.extensions!(argv)
+    {ash_postgres, extensions} = Enum.split_with(extensions, &(&1 == AshPostgres.DataLayer))
 
-    argv
-    |> Ash.Mix.Tasks.Helpers.extensions!()
+    extensions
+    |> check_postgres()
     |> Enum.map(fn extension ->
       if function_exported?(extension, :setup, 1) do
         name =
@@ -28,5 +30,45 @@ defmodule Mix.Tasks.Ash.Setup do
         extension.setup(argv)
       end
     end)
+  end
+
+  defp check_postgres([], extensions), do: extensions
+
+  defp check_postgres(ash_postgres, extensions) do
+    if check_postgres_connectivity(argv) do
+      extensions ++ [ash_postgres]
+    else
+      Mix.shell().error(
+        "Unable to connect to PostgreSQL database. Please ensure your database is running and accessible. Skipping AshPostgres.DataLayer setup."
+      )
+
+      extensions
+    end
+  end
+
+  defp check_postgres_connectivity(argv) do
+    argv
+    |> Ash.Mix.Tasks.Helpers.domains!()
+    |> Enum.flat_map(fn domain ->
+      domain
+      |> Ash.Domain.Info.resources()
+      |> Enum.filter(fn resource ->
+        Ash.DataLayer.data_layer(resource) == AshPostgres.DataLayer
+      end)
+      |> Enum.map(fn resource ->
+        AshPostgres.DataLayer.Info.repo(resource)
+      end)
+    end)
+    |> Enum.uniq()
+    |> Enum.all?(&can_connect?/1)
+  end
+
+  defp can_connect?(repo) do
+    try do
+      {:ok, _} = Ecto.Adapters.SQL.query(repo, "SELECT 1", [])
+      true
+    rescue
+      DBConnection.ConnectionError -> false
+    end
   end
 end
