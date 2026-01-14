113d30511b952b2320dc4b3e618328e342c53d06
wjrtz <33463379+wjrtz@users.noreply.github.com>
2026-01-14 06:05:24 +0100

    feat:  Add `bulk_destroy` step to `AshReactor`. (#2500)

---
+++ lib/ash/reactor/builders/bulk_destroy.ex
@@ -0,0 +1,241 @@
+# SPDX-FileCopyrightText: 2019 ash contributors <https://github.com/ash-project/ash/graphs.contributors>
+#
+# SPDX-License-Identifier: MIT
+
+defimpl Reactor.Dsl.Build, for: Ash.Reactor.Dsl.BulkDestroy do
+  @moduledoc false
+
+  alias Ash.Reactor.BulkDestroyStep
+  alias Ash.Resource.Info
+  alias Reactor.{Argument, Builder}
+  alias Spark.{Dsl.Transformer, Error.DslError}
+  import Ash.Reactor.BuilderUtils
+  import Reactor.Template, only: :macros
+
+  @doc false
+  @impl true
+  def build(bulk_destroy, reactor) do
+    with {:ok, reactor, arguments} <- build_input_arguments(reactor, bulk_destroy) do
+      initial = %Argument{name: :initial, source: bulk_destroy.initial}
+
+      notification_metadata =
+        case bulk_destroy.notification_metadata do
+          template when is_template(template) ->
+            %Argument{name: :notification_metadata, source: template}
+
+          map when is_map(map) ->
+            Argument.from_value(:notification_metadata, map)
+        end
+
+      arguments =
+        arguments
+        |> maybe_append(bulk_destroy.actor)
+        |> maybe_append(bulk_destroy.tenant)
+        |> maybe_append(bulk_destroy.load)
+        |> maybe_append(bulk_destroy.context)
+        |> Enum.concat(bulk_destroy.wait_for)
+        |> Enum.concat([initial, notification_metadata])
+
+      action_options =
+        bulk_destroy
+        |> Map.take([
+          :action,
+          :allow_stream_with,
+          :authorize_changeset_with,
+          :authorize_query_with,
+          :authorize_query?,
+          :authorize?,
+          :batch_size,
+          :domain,
+          :filter,
+          :lock,
+          :max_concurrency,
+          :notify?,
+          :page,
+          :read_action,
+          :resource,
+          :return_errors?,
+          :return_records?,
+          :return_stream?,
+          :rollback_on_error?,
+          :select,
+          :skip_unknown_inputs,
+          :sorted?,
+          :stop_on_error?,
+          :strategy,
+          :stream_batch_size,
+          :stream_with,
+          :success_state,
+          :timeout,
+          :transaction,
+          :undo_action,
+          :undo
+        ])
+        |> Map.put(:return_notifications?, bulk_destroy.notify?)
+        |> Enum.reject(&is_nil(elem(&1, 1)))
+
+      step_options =
+        bulk_destroy |> Map.take([:async?]) |> Map.put(:ref, :step_name) |> Enum.to_list()
+
+      Builder.add_step(
+        reactor,
+        bulk_destroy.name,
+        {BulkDestroyStep, action_options},
+        arguments,
+        step_options
+      )
+    end
+  end
+
+  @doc false
+  @impl true
+  def verify(bulk_destroy, dsl_state) do
+    action_error_path = [:bulk_destroy, bulk_destroy.name, :action]
+
+    with {:ok, action} <-
+           get_action(dsl_state, bulk_destroy.resource, bulk_destroy.action, action_error_path),
+         :ok <-
+           verify_action_type(
+             dsl_state,
+             bulk_destroy.resource,
+             action,
+             :destroy,
+             action_error_path
+           ),
+         :ok <- verify_undo(dsl_state, bulk_destroy),
+         :ok <- maybe_verify_undo_action(dsl_state, bulk_destroy),
+         :ok <- verify_select(dsl_state, bulk_destroy),
+         :ok <- verify_rollback_on_error(dsl_state, bulk_destroy),
+         :ok <- verify_sorted(dsl_state, bulk_destroy) do
+      verify_notify(dsl_state, bulk_destroy)
+    end
+  end
+
+  defguardp is_falsy(value) when value in [nil, false]
+
+  defp verify_notify(dsl_state, bulk_destroy)
+       when bulk_destroy.notify? == true and bulk_destroy.return_stream? == true,
+       do:
+         {:error,
+          DslError.exception(
+            module: Transformer.get_persisted(dsl_state, :module),
+            path: [:bulk_destroy, bulk_destroy.name, :notify?],
+            message: """
+            Setting `notify?` has no effect when `return_stream?` is `true`.
+
+            You must manually consume the resulting stream of records and notifications in a subsequent step.
+            """
+          )}
+
+  defp verify_notify(_dsl_state, _bulk_destroy), do: :ok
+
+  defp verify_sorted(dsl_state, bulk_destroy)
+       when bulk_destroy.sorted? == true and is_falsy(bulk_destroy.return_records?),
+       do:
+         {:error,
+          DslError.exception(
+            module: Transformer.get_persisted(dsl_state, :module),
+            path: [:bulk_destroy, bulk_destroy.name, :sorted?],
+            message: "Setting `sorted?` has no effect with `return_records?` is not `true`."
+          )}
+
+  defp verify_sorted(_dsl_state, _bulk_destroy), do: :ok
+
+  defp verify_rollback_on_error(dsl_state, bulk_destroy)
+       when bulk_destroy.rollback_on_error? == true and is_falsy(bulk_destroy.transaction),
+       do:
+         {:error,
+          DslError.exception(
+            module: Transformer.get_persisted(dsl_state, :module),
+            path: [:bulk_destroy, bulk_destroy.name, :rollback_on_error?],
+            message: "Setting `rollback_on_error?` has no effect when `transaction` is `false`."
+          )}
+
+  defp verify_rollback_on_error(_dsl_state, _bulk_destroy), do: :ok
+
+  defp verify_select(_dsl_state, bulk_destroy) when bulk_destroy.select == [], do: :ok
+  defp verify_select(_dsl_state, bulk_destroy) when bulk_destroy.return_records? == true, do: :ok
+
+  defp verify_select(dsl_state, bulk_destroy),
+    do:
+      {:error,
+       DslError.exception(
+         module: Transformer.get_persisted(dsl_state, :module),
+         path: [:bulk_destroy, bulk_destroy.name, :select],
+         message: "Setting `select` has no effect when `return_records?` is not `true`."
+       )}
+
+  defp verify_undo(dsl_state, bulk_destroy)
+       when bulk_destroy.undo != :never and bulk_destroy.return_stream? == true,
+       do:
+         {:error,
+          DslError.exception(
+            module: Transformer.get_persisted(dsl_state, :module),
+            path: [:bulk_destroy, bulk_destroy.name, :undo],
+            message:
+              "Cannot set undo to anything other than `:never` when `return_stream?` is `true`."
+          )}
+
+  defp verify_undo(_dsl_state, _bulk_destroy), do: :ok
+
+  defp maybe_verify_undo_action(_dsl_state, bulk_destroy) when bulk_destroy.undo == :never,
+    do: :ok
+
+  defp maybe_verify_undo_action(dsl_state, bulk_destroy) do
+    error_path = [:bulk_destroy, bulk_destroy.name, :undo_action]
+
+    with {:ok, action} <-
+           get_action(dsl_state, bulk_destroy.resource, bulk_destroy.undo_action, error_path),
+         :ok <- verify_action_type(dsl_state, bulk_destroy.resource, action, :action, error_path) do
+      verify_action_takes_bulk_result(dsl_state, bulk_destroy.resource, action, error_path)
+    end
+  end
+
+  defp get_action(dsl_state, resource, action_name, error_path) do
+    case Info.action(resource, action_name) do
+      nil ->
+        {:error,
+         DslError.exception(
+           module: Transformer.get_persisted(dsl_state, :module),
+           path: error_path,
+           message:
+             "No action found matching the name `#{action_name}` on resource `#{inspect(resource)}`."
+         )}
+
+      action when is_struct(action) ->
+        {:ok, action}
+    end
+  end
+
+  defp verify_action_type(_dsl_state, _resource, action, action_type, _error_path)
+       when action.type == action_type,
+       do: :ok
+
+  defp verify_action_type(dsl_state, resource, action, action_type, error_path) do
+    {:error,
+     DslError.exception(
+       module: Transformer.get_persisted(dsl_state, :module),
+       path: error_path,
+       message:
+         "Expected the action `#{inspect(action.name)}` on `#{inspect(resource)}` to be a #{action_type}, however it is a #{action.type}"
+     )}
+  end
+
+  defp verify_action_takes_bulk_result(
+         _dsl_state,
+         _resource,
+         %{arguments: [%{name: :bulk_result}]},
+         _error_path
+       ),
+       do: :ok
+
+  defp verify_action_takes_bulk_result(dsl_state, _resource, _action, error_path) do
+    {:error,
+     DslError.exception(
+       module: Transformer.get_persisted(dsl_state, :module),
+       path: error_path,
+       message:
+         "The undo action for a bulk destroy step should take a single `bulk_result` argument."
+     )}
+  end
+end
---
+++ lib/ash/reactor/dsl/bulk_destroy.ex
@@ -0,0 +1,339 @@
+# SPDX-FileCopyrightText: 2019 ash contributors <https://github.com/ash-project/ash/graphs.contributors>
+#
+# SPDX-License-Identifier: MIT
+
+defmodule Ash.Reactor.Dsl.BulkDestroy do
+  @moduledoc """
+  The `bulk_destroy` entity for the `Ash.Reactor` reactor extension.
+  """
+
+  @bulk_actions_default_to_errors? Application.compile_env(
+                                     :ash,
+                                     :bulk_actions_default_to_errors?,
+                                     false
+                                   )
+
+  defstruct __identifier__: nil,
+            action_step?: true,
+            action: nil,
+            actor: [],
+            allow_stream_with: :keyset,
+            async?: true,
+            authorize_changeset_with: :filter,
+            authorize_query_with: :filter,
+            authorize_query?: true,
+            authorize?: nil,
+            batch_size: nil,
+            context: nil,
+            description: nil,
+            domain: nil,
+            filter: %{},
+            guards: [],
+            initial: nil,
+            inputs: [],
+            load: nil,
+            lock: nil,
+            max_concurrency: 0,
+            name: nil,
+            notification_metadata: %{},
+            notify?: false,
+            page: [],
+            read_action: nil,
+            resource: nil,
+            return_errors?: @bulk_actions_default_to_errors?,
+            return_records?: false,
+            return_stream?: false,
+            rollback_on_error?: true,
+            select: [],
+            skip_unknown_inputs: [],
+            sorted?: false,
+            stop_on_error?: @bulk_actions_default_to_errors?,
+            strategy: [:atomic],
+            stream_batch_size: nil,
+            stream_with: nil,
+            success_state: :success,
+            tenant: [],
+            timeout: :infinity,
+            transaction: false,
+            transform: nil,
+            type: :bulk_destroy,
+            undo_action: nil,
+            undo: :never,
+            wait_for: [],
+            __spark_metadata__: nil
+
+  @type t :: %__MODULE__{
+          __identifier__: any,
+          action_step?: true,
+          action: atom,
+          actor: nil | Ash.Reactor.Dsl.Actor.t(),
+          allow_stream_with: :keyset | :offset | :full_read,
+          async?: boolean,
+          authorize_changeset_with: :filter | :error,
+          authorize_query_with: :filter | :error,
+          authorize_query?: boolean,
+          authorize?: boolean | nil,
+          batch_size: nil | pos_integer(),
+          context: nil | Ash.Reactor.Dsl.Context.t(),
+          description: String.t() | nil,
+          domain: Ash.Domain.t(),
+          filter:
+            %{optional(String.t()) => %{required(String.t()) => String.t() | number | boolean}}
+            | Keyword.t(Keyword.t(String.t() | number | boolean)),
+          guards: [Reactor.Guard.Build.t()],
+          initial: Reactor.Template.t(),
+          inputs: [Ash.Reactor.Dsl.Inputs.t()],
+          load: nil | Ash.Reactor.Dsl.ActionLoad.t(),
+          lock: nil | Ash.DataLayer.lock_type(),
+          max_concurrency: non_neg_integer(),
+          name: atom,
+          notification_metadata: map | Reactor.Template.t(),
+          notify?: boolean,
+          page: Keyword.t(),
+          read_action: atom,
+          resource: module,
+          return_errors?: boolean,
+          return_records?: boolean,
+          return_stream?: boolean,
+          rollback_on_error?: boolean,
+          select: [atom],
+          skip_unknown_inputs: list(atom | String.t()),
+          sorted?: boolean,
+          stop_on_error?: boolean,
+          strategy: :atomic | :atomic_batches | :stream,
+          stream_batch_size: nil | pos_integer(),
+          stream_with: nil | :keyset | :offset | :full_read,
+          success_state: :success | :partial_success,
+          tenant: nil | Ash.Reactor.Dsl.Tenant.t(),
+          timeout: nil | timeout,
+          transaction: :all | :batch | false,
+          type: :bulk_destroy,
+          undo_action: nil,
+          undo: :never,
+          __spark_metadata__: Spark.Dsl.Entity.spark_meta()
+        }
+
+  @doc false
+  def __entity__,
+    do: %Spark.Dsl.Entity{
+      name: :bulk_destroy,
+      describe: """
+      Declares a step which will call a destroy action on a resource with a collection of inputs.
+
+      > ### Check the docs! {: .warning}
+      >
+      > Make sure to thoroughly read and understand the documentation in `Ash.bulk_destroy/4` before using.  Read each option and note the default values.  By default, bulk destroys don't return records or errors, and don't emit notifications.
+
+      Caveats/differences from `Ash.bulk_destroy/4`:
+
+      1. `max_concurrency` specifies the number of tasks that Ash will start to process batches, and has no effect on Reactor concurrency targets.  It's could be possible to create a very large number of processes if a number of steps are running bulk actions with a high degree of concurrency.
+      2. Setting `notify?` to `true` will cause both `notify?` and `return_notifications?` to be set to true in the underlying call to `Ash.bulk_destroy/4`. Notifications will then be managed by the `Ash.Reactor.Notifications` Reactor middleware.
+      3. If you specify an undo action it must be a generic action which takes the bulk result as it's only argument.
+
+      #{Ash.Reactor.Dsl.Action.__shared_undo_docs__()}
+      """,
+      examples: [
+        """
+        bulk_destroy :destroy_posts, MyApp.Post, :destroy do
+          initial input(:posts),
+          actor result(:get_user)
+        end
+        """
+      ],
+      no_depend_modules: [:domain, :resource],
+      target: __MODULE__,
+      args: [:name, :resource, {:optional, :action}],
+      identifier: :name,
+      imports: [Reactor.Dsl.Argument],
+      entities: [
+        actor: [Ash.Reactor.Dsl.Actor.__entity__()],
+        context: [Ash.Reactor.Dsl.Context.__entity__()],
+        guards: [Reactor.Dsl.Guard.__entity__(), Reactor.Dsl.Where.__entity__()],
+        inputs: [Ash.Reactor.Dsl.Inputs.__entity__()],
+        tenant: [Ash.Reactor.Dsl.Tenant.__entity__()],
+        wait_for: [Reactor.Dsl.WaitFor.__entity__()]
+      ],
+      singleton_entity_keys: [:actor, :context, :tenant],
+      recursive_as: :steps,
+      schema:
+        [
+          allow_stream_with: [
+            type: {:in, [:keyset, :offset, :full_read]},
+            doc:
+              "The 'worst' strategy allowed to be used to fetch records if the :stream strategy is chosen. See the `Ash.stream!/2` docs for more.",
+            required: false,
+            default: :keyset
+          ],
+          authorize_changeset_with: [
+            type: {:in, [:filter, :error]},
+            doc:
+              "If set to `:error`, instead of filtering unauthorized changes, unauthorized changes will raise an appropriate forbidden error",
+            required: false,
+            default: :filter
+          ],
+          authorize_query_with: [
+            type: {:in, [:filter, :error]},
+            doc:
+              "If set to `:error`, instead of filtering unauthorized query results, unauthorized query results will raise an appropriate forbidden error",
+            required: false,
+            default: :filter
+          ],
+          authorize_query?: [
+            type: :boolean,
+            doc:
+              "If a query is given, determines whether or not authorization is run on that query.",
+            required: false,
+            default: true
+          ],
+          batch_size: [
+            type: {:or, [nil, :pos_integer]},
+            doc:
+              "The number of records to include in each batch. Defaults to the `default_limit` or `max_page_size` of the action, or 100.",
+            required: false
+          ],
+          filter: [
+            type: {:or, [:map, :keyword_list]},
+            doc: "A filter to apply to records. This is also applied to a stream of inputs.",
+            required: false
+          ],
+          initial: [
+            type: Reactor.Template.type(),
+            required: true,
+            doc:
+              "A query or collection of records to destroy. If a query is provided, records matching the query will be destroyed. If a collection is provided, it must implement the `Enumerable` protocol."
+          ],
+          lock: [
+            type: :any,
+            doc: "A lock statement to add onto the query.",
+            required: false
+          ],
+          max_concurrency: [
+            type: :non_neg_integer,
+            doc:
+              "If set to a value greater than 0, up to that many tasks will be started to run batches asynchronously.",
+            required: false,
+            default: 0
+          ],
+          notification_metadata: [
+            type: {:or, [Reactor.Template.type(), :map]},
+            doc:
+              "Metadata to be merged into the metadata field for all notifications sent from this operation.",
+            required: false,
+            default: %{}
+          ],
+          notify?: [
+            type: :boolean,
+            doc:
+              "Whether or not to generate any notifications. This may be intensive for large bulk actions.",
+            required: false,
+            default: false
+          ],
+          page: [
+            type: :keyword_list,
+            doc: "Pagination options, see `Ash.read/2` for more.",
+            required: false,
+            default: []
+          ],
+          read_action: [
+            type: :atom,
+            doc: "The action to use when building the read query.",
+            required: false
+          ],
+          return_errors?: [
+            type: :boolean,
+            doc:
+              "Whether or not to return all of the errors that occur. Defaults to false to account for large destroys.",
+            required: false,
+            default: @bulk_actions_default_to_errors?
+          ],
+          return_records?: [
+            type: :boolean,
+            doc:
+              "Whether or not to return all of the records that were destroyed. Defaults to false to account for large destroys.",
+            required: false,
+            default: false
+          ],
+          return_stream?: [
+            type: :boolean,
+            doc: "If set to `true`, instead of an `Ash.BulkResult`, a mixed stream is returned.",
+            required: false,
+            default: false
+          ],
+          rollback_on_error?: [
+            type: :boolean,
+            doc:
+              "Whether or not to rollback the transaction on error, if the resource is in a transaction.",
+            required: false,
+            default: true
+          ],
+          select: [
+            type: {:wrap_list, :atom},
+            doc:
+              "A select statement to apply to records. Ignored if `return_records?` is not `true`.",
+            required: false
+          ],
+          skip_unknown_inputs: [
+            type: {:wrap_list, {:or, [:atom, :string]}},
+            doc:
+              "A list of inputs that, if provided, will be ignored if they are not recognized by the action. Use `:*` to indicate all unknown keys.",
+            required: false
+          ],
+          sorted?: [
+            type: :boolean,
+            doc:
+              "Whether or not to sort results by their input position, in cases where `return_records?` is set to `true`.",
+            required: false,
+            default: false
+          ],
+          stop_on_error?: [
+            type: :boolean,
+            doc:
+              "If `true`, the first encountered error will stop the action and be returned. Otherwise, errors will be skipped.",
+            required: false,
+            default: @bulk_actions_default_to_errors?
+          ],
+          strategy: [
+            type: {:list, {:in, [:atomic, :atomic_batches, :stream]}},
+            doc:
+              "The strategy or strategies to enable. `:stream` is used in all cases if the data layer does not support atomics.",
+            required: false,
+            default: [:atomic]
+          ],
+          stream_batch_size: [
+            type: :pos_integer,
+            doc: "Batch size to use if provided a query and the query must be streamed.",
+            required: false
+          ],
+          stream_with: [
+            type: {:in, [:keyset, :offset, :full_read]},
+            doc:
+              "The specific strategy to use to fetch records. See `Ash.stream!/2` docs for more.",
+            required: false
+          ],
+          success_state: [
+            type: {:in, [:success, :partial_success]},
+            doc:
+              "Bulk results can be entirely or partially successful. Chooses the `Ash.BulkResult` state to consider the step a success.",
+            required: false,
+            default: :success
+          ],
+          timeout: [
+            type: :timeout,
+            doc:
+              "If none is provided, the timeout configured on the domain is used (which defaults to `30_000`).",
+            required: false
+          ],
+          transaction: [
+            type: {:in, [:all, :batch, false]},
+            doc:
+              "Whether or not to wrap the entire execution in a transaction, each batch, or not at all.",
+            required: false,
+            default: :batch
+          ]
+        ]
+        |> Spark.Options.merge(
+          Ash.Reactor.Dsl.Action.__shared_action_option_schema__(),
+          "Shared action options"
+        )
+    }
+end
