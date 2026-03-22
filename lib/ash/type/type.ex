cba45061f682ab40fd95d31bfbfed6d7d11df81e
Torkild Gundersen Kjevik <torkild.kjevik@boitano.no>
2026-03-22 13:18:09 +0100

    feat: add auto type resolution for expression calculations

--- lib/ash/type/type.ex
+++ lib/ash/type/type.ex
@@ -2190,6 +2190,10 @@ defmodule Ash.Type do
   defp vagueness(_), do: 0
 
   @doc false
+  def set_type_transformation(%{type: :auto} = thing) do
+    {:ok, thing}
+  end
+
   def set_type_transformation(%{type: original_type, constraints: constraints} = thing) do
     type = get_type!(original_type)
 
