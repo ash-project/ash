c96e1c21ef16cc5ba25896e4d41a24b041637310
Rebecca Le <543859+sevenseacat@users.noreply.github.com>
2026-05-23 06:26:32 +0800

    docs: Add section about generated subject helper functions to Code Interface docs (#2715)

--- lib/ash/resource/record.ex
+++ lib/ash/resource/record.ex
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2026 ash contributors <https://github.com/ash-project/ash/graphs/contributors>
+#
+# SPDX-License-Identifier: MIT
+
 defmodule Ash.Resource.Record do
   @moduledoc false
   @type t :: struct()
