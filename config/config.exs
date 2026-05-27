fc1f2d4dd49858692bd71d9653b78cab652938d6
Philip Munksgaard <philip@munksgaard.me>
2026-05-27 14:44:02 +0200

    test: Remove the setting of include_embedded_source_by_default
    This means that the test from f7f5ad46c always passes, but at least now the
    trace of the three commits is reproducible.

--- config/config.exs
+++ config/config.exs
@@ -60,5 +60,4 @@ if config_env() == :test do
   config :elixir, :time_zone_database, Tz.TimeZoneDatabase
 
   config :ash, Ash.Type.UUIDv7, match_v4_uuids?: true
-  config :ash, :include_embedded_source_by_default?, false
 end
