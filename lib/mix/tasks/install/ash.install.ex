2d5e9edf49935cfff9349e2201c00f73c49004b6
Zach Daniel <zach@zachdaniel.dev>
2026-06-15 16:07:15 -0400

    improvement: add explanatory comment on backwards compat config

--- lib/mix/tasks/install/ash.install.ex
+++ lib/mix/tasks/install/ash.install.ex
@@ -173,72 +173,31 @@ if Code.ensure_loaded?(Igniter) do
         @setup_backwards_compatibility,
         fn igniter ->
           Igniter.Scribe.patch(igniter, fn igniter ->
-            igniter
-            |> Igniter.Project.Config.configure(
+            Igniter.Project.Config.configure_group(
+              igniter,
               "config.exs",
               :ash,
-              [:allow_forbidden_field_for_relationships_by_default?],
-              true
-            )
-            |> Igniter.Project.Config.configure(
-              "config.exs",
-              :ash,
-              [:include_embedded_source_by_default?],
-              false
-            )
-            |> Igniter.Project.Config.configure(
-              "config.exs",
-              :ash,
-              [:show_keysets_for_all_actions?],
-              false
-            )
-            |> Igniter.Project.Config.configure(
-              "config.exs",
-              :ash,
-              [:default_page_type],
-              :keyset
-            )
-            |> Igniter.Project.Config.configure(
-              "config.exs",
-              :ash,
-              [:policies, :no_filter_static_forbidden_reads?],
-              false
-            )
-            |> Igniter.Project.Config.configure(
-              "config.exs",
-              :ash,
-              [:keep_read_action_loads_when_loading?],
-              false
-            )
-            |> Igniter.Project.Config.configure(
-              "config.exs",
-              :ash,
-              [:default_actions_require_atomic?],
-              true
-            )
-            |> Igniter.Project.Config.configure(
-              "config.exs",
-              :ash,
-              [:read_action_after_action_hooks_in_order?],
-              true
-            )
-            |> Igniter.Project.Config.configure(
-              "config.exs",
-              :ash,
-              [:bulk_actions_default_to_errors?],
-              true
-            )
-            |> Igniter.Project.Config.configure(
-              "config.exs",
-              :ash,
-              [:transaction_rollback_on_error?],
-              true
-            )
-            |> Igniter.Project.Config.configure(
-              "config.exs",
-              :ash,
-              [:redact_sensitive_values_in_errors?],
-              true
+              [],
+              [
+                {[:allow_forbidden_field_for_relationships_by_default?], true},
+                {[:include_embedded_source_by_default?], false},
+                {[:show_keysets_for_all_actions?], false},
+                {[:default_page_type], :keyset},
+                {[:policies, :no_filter_static_forbidden_reads?], false},
+                {[:keep_read_action_loads_when_loading?], false},
+                {[:default_actions_require_atomic?], true},
+                {[:read_action_after_action_hooks_in_order?], true},
+                {[:bulk_actions_default_to_errors?], true},
+                {[:transaction_rollback_on_error?], true},
+                {[:redact_sensitive_values_in_errors?], true}
+              ],
+              comment: """
+              These enable behaviors that will become the default in the next major
+              version of Ash. Setting them now opts your application into the new
+              behavior and ensures a seamless upgrade. See the backwards compatibility
+              guide for an explanation of each setting:
+              https://hexdocs.pm/ash/backwards-compatibility-config.html
+              """
             )
           end)
         end
